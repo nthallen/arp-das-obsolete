c2c556dc1febbcbf9f7a1b7c5a496e481a12094a
Eileen Schomp <eil@huarp.harvard.edu>
1992-07-16 14:48:35 +0000

    Initial revision

---
+++ QNX4/eilsrc/das/dfs/lib/dbr_info.c
@@ -0,0 +1,10 @@
+/* dbr_info.c simply defines variable dbr_info in one place so
+   it may easily be included in the library whether needed for
+   DG or DC purposes.
+   Written 5/24/91
+   Modified 5/13/92 by Eil.
+*/
+#include "dbr.h"
+
+dbr_info_type dbr_info = {0};
+
