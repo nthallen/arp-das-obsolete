69517fcaa8353821e15a2a40871315cf46acb02b
Eileen Schomp <eil@huarp.harvard.edu>
1992-07-16 14:48:14 +0000

    general update

--- QNX4/eilsrc/das/dfs/lib/db.c
+++ QNX4/eilsrc/das/dfs/lib/db.c
@@ -9,11 +9,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <assert.h>
 #include <sys/kernel.h>
 #include <sys/sendmx.h>
 #include <sys/name.h>
 #include <sys/sched.h>
 #include <sys/types.h>
+#include <sys/psinfo.h>
 #include <malloc.h>
 #include <sys/sendmx.h>
 #include <dac.h>
@@ -25,123 +27,82 @@
 int db_id;
 
 /* function declarations */
-static int init_client_2(int who);
-static void db_forward(pid_t who, unsigned char msg_type, unsigned short seq,
-		       unsigned char n_rows, void *other, unsigned char n_rows1, void *other1);
+static int init_client_2(pid_t who);
+static void db_forward(pid_t who, msg_hdr_type msg_type,
+       token_type n_rows, void *other, token_type n_rows1, void *other1);
 
 int DB_init() {
 /* intialises the buf task as the buf task */	
 char name[FILENAME_MAX+1];
 
 /* initialise error handling if the user didn't */
-if (!msg_initialised()) msg_init(DB_NAME,0,1,0,0,1);
+if (!msg_initialised()) msg_init(DB_NAME,0,1,0,0,1,1);
   
 /* attach name */
 if ((db_id=qnx_name_attach(getnid(),LOCAL_SYMNAME(DB_NAME,name)))==-1)
     msg(MSG_EXIT_ABNORM,"Can't attach name %s",name);  
 
-num_clients=0;
+if ( (qnx_pflags(~0,_PPF_PRIORITY_REC,0,0)) == -1)
+    msg(MSG_EXIT_ABNORM,"Can't set receiving priority order");
+
+/* num_clients=0; */
 
 return 0;	
 }
 
-void DB_s_data(int who, unsigned short seq, unsigned short n_rows,
-       unsigned char *data, unsigned short n_rows1, unsigned char *data1) {
+void DB_s_data(pid_t who, token_type n_rows,
+       unsigned char *data, token_type n_rows1, unsigned char *data1) {
 
     /* sends the data back to the bclient */
     if (!n_rows) return;
-    db_forward(who,DCDATA,seq,n_rows,data,n_rows1, data1);
+    db_forward(who,DCDATA,n_rows,data,n_rows1, data1);
 }
 
 
-void DB_s_tstamp(int who, unsigned short seq, tstamp_type *tstamp) {
+void DB_s_tstamp(pid_t who, tstamp_type *tstamp) {
     /* sends the stamp back to the bclient */	
-    db_forward(who, TSTAMP, seq, 0, tstamp, 0,0);
+    db_forward(who, TSTAMP, 0, tstamp, 0,0);
 }
 
 
-void DB_s_dascmd(int who, unsigned short seq, unsigned char type,
+void DB_s_dascmd(pid_t who, unsigned char type,
 		 unsigned char val) {
 dascmd_type dasc;
 
     dasc.type=type;
     dasc.val=val;
     /* sends the cmd back to the bclient */	
-    db_forward(who, DCDASCMD, seq, 0, &dasc,0,0);
+    db_forward(who, DCDASCMD, 0, &dasc,0,0);
 }
 
-/* some signals other than DC signals are special for DB */
-int DB_operate(int who, unsigned char *msg_ptr) {
-unsigned char r = DAS_UNKN;
-struct db_msg_type {
-    msg_header_type hdr;
-    db_request_type req;
-} *rq;
-
-rq=(struct db_msg_type *)msg_ptr;
-
-  switch (rq->hdr.msg_type) {          	
-    case REQUEST:
-	/* check for null request */
-	if (!rq->req.min_rows && !rq->req.max_rows) num_clients--;
-	else DB_get_data(who, rq->hdr.seq, rq->req.min_rows, rq->req.max_rows);
-    break;
-    case DCINIT:
-	init_client_2(who);
-    break;
-    default: msg(MSG_WARN,"unknown msg received from %d with type %d",who,rq->hdr.msg_type);
-	Reply(who, &r, 1);
-  }
-  return 0;
-}
-
-
-static int init_client_2(int who) {
-/* initialise a buffered client */
-unsigned char rv = DAS_OK;
-struct _mxfer_entry mlist[2];
-int next_tid;
-
-  next_tid = dbr_info.next_tid;
-  dbr_info.next_tid=getpid();
-  _setmx(&mlist[0],&rv, 1);
-  _setmx(&mlist[1],&dbr_info,sizeof(dbr_info));
-  if (!Replymx(who, 2, mlist))
-    num_clients++;
-  dbr_info.next_tid=next_tid;
-
-  return 0;
-}
-
-static void db_forward(pid_t who, unsigned char msg_type, unsigned short seq,
-		       unsigned char n_rows, void *other, unsigned char n_rows1, void *other1) {
-static struct _mxfer_entry slist[5];
-unsigned char tmp;
+static void db_forward(pid_t who, msg_hdr_type msg_type,
+       token_type n_rows, void *other, token_type n_rows1, void *other1) {
+static struct _mxfer_entry slist[4];
+token_type tmp;
 int scount;
 
-  _setmx(&slist[0],&msg_type,1);
-  _setmx(&slist[1],&seq, 2);
-  scount=3;
+  _setmx(&slist[0],&msg_type,sizeof(msg_hdr_type));
+  scount=2;
   switch(msg_type) {
     case DCDATA:
 	assert(n_rows);
 	tmp=n_rows;
 	if (other1 && n_rows1) tmp+=n_rows1;
-	_setmx(&slist[2],&tmp,1);
-	_setmx(&slist[3],other,n_rows*tmi(nbrow));
-	scount=4;
+	_setmx(&slist[1],&tmp,sizeof(token_type));
+	_setmx(&slist[2],other,n_rows*tmi(nbrow));
+	scount=3;
 	if (other1 && n_rows1) {
-	_setmx(&slist[4],other1,n_rows1*tmi(nbrow));
-	    scount=5;
+	_setmx(&slist[3],other1,n_rows1*tmi(nbrow));
+	    scount=4;
 	}
 	break;
     case TSTAMP:
-	_setmx(&slist[2],other,sizeof(tstamp_type));	
+	_setmx(&slist[1],other,sizeof(tstamp_type));	
 	break;
     case DCDASCMD:
-	_setmx(&slist[2],other,sizeof(dascmd_type));	
+	_setmx(&slist[1],other,sizeof(dascmd_type));	
 	break;
-    default: msg(MSG_EXIT_ABNORM,"in db_forward, type %d",msg_type);
+    default: msg(MSG_EXIT_ABNORM,"in db_forward, bad type %ud",msg_type);
   }
 
   if (Replymx(who, scount, slist)==-1)
