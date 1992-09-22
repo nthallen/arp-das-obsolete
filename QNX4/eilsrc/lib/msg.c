49c4af53bcbe84deddc6cb808164823a416103fd
Eileen Schomp <eil@huarp.harvard.edu>
1992-09-22 13:44:20 +0000

    Console functions and timestamping to files added.

--- QNX4/eilsrc/lib/msg.c
+++ QNX4/eilsrc/lib/msg.c
@@ -12,6 +12,7 @@
 #include <sys/dev.h>
 #include <errno.h>
 #include <process.h>
+#include <time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/qnxterm.h>
@@ -24,11 +25,19 @@
 #include <msg.h>
 #include <sounds.h>
 
+/* IBM compatable display attributes for console functions */
+#define IBM_NORMAL 0x07
+#define IBM_REVERSE 0x70
+#define IBM_BLINK (IBM_NORMAL | 0x80)
+
+#define LEN 300
+
 static int msg_inited;
 static int msg_verbose = 1;
 static int msg_memo_node = -1;
 static FILE *msg_fp = 0;
 static int msg_devfd = 0;
+static struct _console_ctrl *msg_cc = NULL;
 static int msg_row = 0;
 static int msg_col = 0;
 static int msg_size = 0;
@@ -40,6 +49,9 @@ static int msg_sounds = 1;
 static int msg_sys = 1;
 static int scrwidth = 0;
 static pid_t memo_tid;
+static char *msg_buf = 0;
+static char *msg_fbuf = 0;
+static time_t timevar;
 
 /* returns true if messages already initialised */
 int msg_initialised(void) {
@@ -51,6 +63,9 @@ int msg_initialised(void) {
 void msg_end() {
 	if (msg_fp) fclose(msg_fp);
 	if (msg_devfd) close(msg_devfd);
+	if (msg_cc) console_close(msg_cc);
+	if (msg_buf) free(msg_buf);
+	if (msg_fbuf) free(msg_fbuf);
 	msg_inited=0;
 }
 
@@ -58,7 +73,7 @@ void msg_end() {
 void msg(int fatal, char *format,...) {
 
 va_list ap;
-char errline[300] = {MEMO_MSG,'\0'};
+char errline[LEN] = {MEMO_MSG,'\0'};
 char *p;
 reply_type replymsg;
 int msgsize, i;
@@ -70,7 +85,7 @@ unsigned attr;
 	va_start(ap, format);
 	if (msg_hdr)
 		if (strlen(msg_hdr)) {
-			strcat(errline, msg_hdr);
+			strncat(errline, msg_hdr, 10);
 			strcat(errline, ": ");	
 		}
 	if ( fatal>=MSG_EXIT_ABNORM )  strcat(errline,"FATAL: ");
@@ -90,7 +105,9 @@ unsigned attr;
 
 	/* log to file */		
 	if (msg_fp)  {
-	    fprintf(msg_fp,"%s\n",p);
+	    time(&timevar);
+	    strftime(msg_fbuf,10,"%T",localtime(&timevar));
+	    fprintf(msg_fp,"%s: %s\n",msg_fbuf,p);
 	    fflush(msg_fp);
 	}
 
@@ -108,10 +125,21 @@ unsigned attr;
 		case MSG_EXIT_NORM: attr = msg_pass; break; /* exit normally */
 		default: attr = msg_fail;		/* exit abnormally */
 		}
-	    for (i=0;i<msg_size;i++)
-		term_type(msg_row,msg_col+i," ",1,msg_pass);
-	    term_type(msg_row,msg_col,p,(msg_size<=strlen(p))?msg_size:0,attr);
-	    term_flush();
+
+	    if (!msg_cc) {
+		for (i=0;i<msg_size;i++)
+		    term_type(msg_row,msg_col+i," ",1,msg_pass);
+		term_type(msg_row,msg_col,p,(msg_size<=strlen(p))?msg_size:0,attr);
+		term_flush();
+	    } else {
+		console_read(msg_cc,0,2*(msg_row*scrwidth+msg_col),msg_buf,msg_size*2,0,0,0);
+		strnset(msg_buf,32,msg_size*2);	
+		for (i=0;i<(msg_size*2);i+=2,p++)
+		    *(msg_buf+i)=*p;
+		for (i=1;i<=(msg_size*2);i+=2)
+		    *(msg_buf+i)=(char)attr;
+		console_write(msg_cc,0,2*(msg_row*scrwidth+msg_col),msg_buf,msg_size*2,0,0,0);
+	    }
 	}
 	
 	errno=0;
@@ -139,8 +167,7 @@ void msg_init(char *hdr, char *fn, int verbose, nid_t memo_node, char *oarg, int
 	msg_sounds = sounds;
 	msg_memo_node = -1;
 	msg_sys = sys;
-	if (hdr && strlen(hdr))
-	    strncpy(msg_hdr,hdr,39);
+	if (hdr && strlen(hdr)) strncpy(msg_hdr,hdr,39);
 	if (oarg && strlen(oarg)) {
 	    j = strcspn(oarg,",;");
 	    p = oarg+j+1;	
@@ -179,22 +206,37 @@ void msg_init(char *hdr, char *fn, int verbose, nid_t memo_node, char *oarg, int
 	if (fn && strlen(fn))
 	    if ( !(msg_fp = fopen(fn,"a")))
 		msg(MSG_EXIT_ABNORM,"Can't open error file %s",fn);
+	    else {
+		msg_fbuf = malloc(10);
+		time(&timevar);
+		strftime(msg_fbuf,10,"%D",localtime(&timevar));
+		fprintf(msg_fp,"Log Date: %s\n",msg_fbuf);
+		fflush(msg_fp);
+	    }
 	if (devfn && strlen(devfn))
 	    if ( !(msg_devfd = open(devfn,O_RDWR)))
 		msg(MSG_EXIT_ABNORM,"Can't open device %s",devfn);
 	    else {
-		tcsetct(msg_devfd, getpid());
-		if (term_load()) {
+		if ( (msg_cc = console_open(msg_devfd,O_RDWR))) {
+		    console_size(msg_cc,0,0,0,0,&scrwidth);
+		    msg_size = (msg_size <= 0) ? scrwidth-msg_col : msg_size;
+		    msg_buf=(char *)malloc(2*msg_size);
+		    msg_fail = msg_fail ? msg_fail : IBM_BLINK;
+		    msg_warn = msg_warn ? msg_warn : IBM_REVERSE;
+		    msg_pass = msg_pass ? msg_pass : IBM_NORMAL;
+		} else {
+		    tcsetct(msg_devfd, getpid());
+		    if (term_load()) {
 			msg(MSG_EXIT_ABNORM,"Can't load terminal definition");
 			close(msg_devfd);
 			msg_devfd = 0;
-		}
-		else {
-		    scrwidth = term_state.num_cols;
-		    msg_size = (msg_size <= 0) ? scrwidth-msg_col : msg_size;
-		    msg_fail = msg_fail ? msg_fail : TERM_BLINK;
-		    msg_warn = msg_warn ? msg_warn : TERM_INVERSE;
-		    msg_pass = msg_pass ? msg_pass : TERM_NORMAL;
+		    } else {
+			scrwidth = term_state.num_cols;
+			msg_size = (msg_size <= 0) ? scrwidth-msg_col : msg_size;
+			msg_fail = msg_fail ? msg_fail : TERM_BLINK;
+			msg_warn = msg_warn ? msg_warn : TERM_INVERSE;
+			msg_pass = msg_pass ? msg_pass : TERM_NORMAL;
+		    }
 		}
 	    }
 	msg_inited = 1;
