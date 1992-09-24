f5ecbcdcf6097f14de8095b619c58df2114d7f26
Norton Allen <allen@huarp.harvard.edu>
1992-09-24 20:39:32 +0000

    Initial revision

---
+++ QNX4/indexer/ixdrv.h
@@ -0,0 +1,42 @@
+/* ixdrv.h is an include file internal to the indexer driver.
+ * $Log$
+ */
+#ifndef IXDRV_H_INCLUDED
+#define IXDRV_H_INCLUDED
+
+#ifndef INDEXER_H_INCLUDED
+  #error Must include indexer.h before ixdrv.h
+#endif
+
+typedef struct {
+  step_t base_addr;
+  unsigned char EIR;
+  unsigned char scan_bit;
+  unsigned char on_bit;
+  unsigned char supp_bit;
+} chandef;
+
+typedef struct ixcmdlist {
+  struct ixcmdlist *next;
+  ixcmd c;
+} ixcmdl;
+
+typedef struct {
+  ixcmdl *first;
+  ixcmdl *last;
+  step_t online;
+  step_t online_delta;
+  step_t offline;
+  byte_t state;
+  step_t scan_addr;
+  step_t scan_amount;
+  step_t w_amount;
+  step_t to_go;
+} drvstat;
+#define CST_ON_ALT 1
+#define CST_DRIVING 2 /* May not be needed */
+#define IX_SCAN_PROXY 255
+#define IX_CHAN_0_PROXY (IX_SCAN_PROXY-N_CHANNELS)
+
+extern drvstat drive[N_CHANNELS];
+#endif
