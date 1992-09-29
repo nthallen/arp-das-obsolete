ab1a1fd6a586dbffb70e49c8f2edffcc4da73d1e
Eileen Schomp <eil@huarp.harvard.edu>
1992-09-29 13:51:32 +0000

    extracted cc_init_options to another file. changed MSG_WARN to MSG when lookinf for cmdctrl.

--- QNX4/eilsrc/das/lib/cc.c
+++ QNX4/eilsrc/das/lib/cc.c
@@ -7,58 +7,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <assert.h>
+#include <errno.h>
 #include <process.h>
 #include <sys/kernel.h>
 #include <sys/name.h>
 #include <das_utils.h>
 #include <globmsg.h>
 #include <cmdctrl.h>
+#include <symname.h>
+#include <msg.h>
 
-int cc_init_options(int argcc, char *argvv[], unsigned char min_dasc,
-		    unsigned char max_dasc, unsigned char min_msg,
-		    unsigned char max_msg, quit_type how_to_quit) {
-extern char *optarg;
-extern int optind, opterr, optopt;
-int c;
-int action=NOTHING_ON_DEATH;
-char cmd_line[MAX_MSG_SIZE];
-char full[MAX_MSG_SIZE];
-char ts[MAX_MSG_SIZE];
-
-    /* error handling intialisation if the client code didnt */
-    if (!msg_initialised())
-	msg_init(argvv[0],0,1,-1,0,1,1);
-
-    opterr = 0;
-    optind = 0;
-
-	do {
-	c=getopt(argcc,argvv,opt_string);
-	switch (c) {
-		case 'R': action=REBOOT; break;
-		case 'D': action=DAS_RESTART; break;
-		case 'T': action=TASK_RESTART; break;
-		case 'S': action=SHUTDOWN; break;
-		case '?': msg(MSG_EXIT_ABNORM,"Invalid option -%c",optopt);
-		default : break;
-	}
-    }  while (c!=-1);
-    optind = 0;
-    opterr = 1;
-
-    if (action==TASK_RESTART) {
-	if (qnx_fullpath(full,argvv[0])==NULL)
-	    msg(MSG_EXIT_ABNORM,"Can't get fullpath of %s",basename(argvv[0]));
-	if (getcmd(cmd_line)==NULL)
-	    msg(MSG_EXIT_ABNORM,"Can't get command line args");
-	sprintf(ts,"%s %s",full,cmd_line);
-    }
-
-    return (cc_init(min_dasc, max_dasc, min_msg, max_msg, how_to_quit, action, ts));
-
-}
-
+/* returns cmd tid on success */
 int cc_init(unsigned char min_dasc, unsigned char max_dasc,
 	    unsigned char min_msg, unsigned char max_msg,
 	    quit_type how_to_quit, death_type how_to_die,
@@ -70,6 +29,8 @@ ccreg_type reg = {CCReg_MSG};
 char name[FILENAME_MAX+1];
 int i;
 
+    if (!msg_initialised()) msg_init("",0,1,-1,0,1,1);
+
     if (how_to_quit==NOTHING_ON_QUIT && how_to_die==NOTHING_ON_DEATH
 	&& min_dasc==0 && max_dasc==0 && min_msg==0 && max_msg==0)
 	    /* don't bother registering */
@@ -77,11 +38,14 @@ int i;
 
     for (i=0;i<3;i++)
 	if ( (cmd_tid = qnx_name_locate(getnid(),LOCAL_SYMNAME(CMD_CTRL,name),0,0)) == -1) {
-	    if (!i) msg(MSG_WARN,"Im trying to find %s on node %d", name, getnid());
+	    if (!i) msg(MSG,"Im trying to find %s on node %d", name, getnid());
 	    sleep(1);
 	}
 	else break;
-    if (i>0 && i<3) msg(MSG,"Found %s on node %d, continuing...",name,getnid());
+    if (i>0 && i<3) {
+	errno=0;
+	msg(MSG,"Found %s on node %d, continuing...",name,getnid());
+    }
     else if (i>=3) msg(MSG_EXIT_ABNORM,"Couldn't find %s on node %d",name,getnid());
 
     reg.min_dasc = min_dasc;
@@ -104,5 +68,4 @@ int i;
 	msg(MSG_EXIT_ABNORM, "Bad response from cmdctrl");
 
     return(cmd_tid);
-
 }
