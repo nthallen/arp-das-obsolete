449d726e0626629dacfe00d1aefcab8d19a7fd6a
Norton Allen <allen@huarp.harvard.edu>
1992-10-18 19:07:41 +0000

    Remove retries, count on namewait to wait as necessary.

--- QNX4/nortlib/find_name.c
+++ QNX4/nortlib/find_name.c
@@ -1,10 +1,12 @@
 /* nl_find_name() provides a general-purpose approach to finding other
    processes.
-   It will try MAX_RETRIES times with 3 seconds between
-   attempts. If name cannot be found after MAX_RETRIES,
+   If name cannot be found
    nl_error(nl_response,...) is called. nl_find_name() returns the
    pid or -1 on error (if it returns).
  * $Log$
+ * Revision 1.1  1992/10/18  19:05:53  nort
+ * Initial revision
+ *
 */
 #include <sys/types.h>
 #include <sys/name.h>
@@ -12,17 +14,11 @@
 #include "nortlib.h"
 static char rcsid[] = "$Id$";
 
-#define MAX_RETRIES 2
-
 pid_t nl_find_name(nid_t node, char *name) {
   int i;
   pid_t pid;
 
-  for (i = 0; ;) {
-	pid = qnx_name_locate(node, name, 0, 0);
-	if (pid != -1 || ++i >= MAX_RETRIES) break;
-	sleep(3);
-  }
+  pid = qnx_name_locate(node, name, 0, 0);
   if (pid == -1 && nl_response)
 	nl_error(nl_response, "Unable to locate %s", name);
   return(pid);
