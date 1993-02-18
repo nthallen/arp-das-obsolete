2c6c2784d115f6ec73ed9291ce0b02bde4c7afec
Norton Allen <allen@huarp.harvard.edu>
1993-02-18 02:28:36 +0000

    Simplified via use of indxr_cmd()

--- QNX4/nortlib/ixdrive.c
+++ QNX4/nortlib/ixdrive.c
@@ -2,23 +2,17 @@
  * Calls send_CC() to send message to indexer. Returns:
  *  -1 if nl_response != NLRSP_DIE and CC not present
  *  DAS_OK on success
- *  DAS_UNKN if indexer isn't present
+ *  DAS_UNKN if indexer isn't present && nl_response != NLRSP_DIE
  * $Log$
+ * Revision 1.1  1992/09/02  20:16:15  nort
+ * Initial revision
+ *
 */
 #include "nortlib.h"
 #include "indexer.h"
 static char rcsid[] = "$Id$";
 
 int indxr_drive(byte_t drive, byte_t dir, step_t steps) {
-  idxr_msg im;
-  int rv;
-  
-  im.msgcode = INDEXER_MSG;
-  im.dir_scan = dir & IX_DIR;
-  im.drive = drive;
-  im.steps = steps;
-  rv = send_CC(&im, sizeof(im), 0);
-  if (rv == 0) rv = im.msgcode;
-  return(rv);
+  return(indxr_cmd(dir & IX_DIR, drive, steps, 0));
 }
 
