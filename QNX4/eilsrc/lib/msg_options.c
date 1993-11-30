c2ada14799c42b70f6896130e89ac34c1ea586f6
Eileen Schomp <eil@huarp.harvard.edu>
1993-11-30 20:00:30 +0000

    added debug levels

--- QNX4/eilsrc/lib/msg_options.c
+++ QNX4/eilsrc/lib/msg_options.c
@@ -14,14 +14,15 @@ extern char *optarg;
 extern int optind, opterr, optopt;
 
     int c;
-    int verbose, sounds, sys;
+    int verbose, sounds, sys, level;
     char errfilename[NAME_MAX] = {'\0'};
     char oarg[80] = {'\0'};
     char targ[NAME_MAX] = {'\0'};
     char hdr[40] = {'\0'};
 
     verbose = sounds = 1;
-    msg_init(default_hdr,0,1,0,0,1,1);
+    level = 0;
+    msg_init(default_hdr,0,1,0,0,1,1,-1);
     if (default_hdr) strncpy(hdr,default_hdr,39);
     opterr = 0;
     optind = 0;
@@ -35,10 +36,11 @@ extern int optind, opterr, optopt;
 	 	case 'v': verbose = 0; break;
 		case 's': sounds = 0; break;
 		case 'y': sys = 0; break;
+		case 'l': level++; break;
 		case '?': msg(MSG_EXIT_ABNORM,"Invalid option -%c",optopt);
 		default : break;
 	}
     }  while (c!=-1);
-    msg_init(hdr,errfilename,verbose,targ,oarg,sounds,sys);
+    msg_init(hdr,errfilename,verbose,targ,oarg,sounds,sys,level);
     opterr = 1;
 }
