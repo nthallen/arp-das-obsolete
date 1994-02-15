1ca63092dab4971e85ce7f21aa977b6d4797a0f3
Norton Allen <allen@huarp.harvard.edu>
1994-02-15 19:01:55 +0000

    Added -p option from tma.c Improved syntax error reporting

--- QNX4/nortlib/cic.c
+++ QNX4/nortlib/cic.c
@@ -1,5 +1,8 @@
 /* cic.c Defines functions used by Command Interpreter Clients
  * $Log$
+ * Revision 1.3  1993/09/15  19:25:53  nort
+ * Using nl_make_name()
+ *
  * Revision 1.2  1993/07/01  15:35:04  nort
  * Eliminated "unreferenced" via Watcom pragma
  *
@@ -26,6 +29,7 @@ static nid_t cis_node = 0;
 static unsigned char cic_msg_type;
 static unsigned short cic_reply_code;
 static struct _mxfer_entry cic_msgs[3], cic_reply;
+static int playback = 0;
 
 void cic_options(int argcc, char **argvv, const char *def_prefix) {
   int c;
@@ -47,6 +51,9 @@ void cic_options(int argcc, char **argvv, const char *def_prefix) {
 		strncpy(cic_header, optarg, CMD_PREFIX_MAX);
 		cic_header[CMD_PREFIX_MAX-1] = '\0';
 		break;
+	  case 'p':
+		playback = 1;
+		break;
 	  case '?':
 		nl_error(3, "Unknown option -%c", optopt);
 	  default:
@@ -64,7 +71,7 @@ void cic_options(int argcc, char **argvv, const char *def_prefix) {
    response is required. Returns zero on success.
 */
 int cic_init(void) {
-  cis_pid = nl_find_name(cis_node, nl_make_name(CMDINTERP_NAME));
+  cis_pid = nl_find_name(cis_node, nl_make_name(CMDINTERP_NAME, 0));
   if (cis_pid == -1) {
 	cis_pid = 0;
 	return(1);
@@ -116,23 +123,37 @@ int cic_query(char *version) {
 
 /* ci_sendcmd() Sends a command to the CIS.
    If cmdtext==NULL, sends CMDINTERP_QUIT,0
+    mode == 0 ==> CMDINTERP_SEND
+	mode == 1 ==> CMDINTERP_TEST
+	mode == 2 ==> CMDINTERP_SEND_QUIET
    Possible errors:
 	 Unable to locate CIS: Normally fatal: return 1
 	 CMDREP_QUIT from CIS: Reset cis_pid: return it
 	 CMDREP_SYNERR from CIS: Normally fatal: return it
 	 CMDREP_EXECERR from CIS: Normally warning: return it
 */
-int ci_sendcmd(const char *cmdtext, int test) {
+int ci_sendcmd(const char *cmdtext, int mode) {
   unsigned sparts, clen;
   int rv;
   
-  if (cis_pid == 0 && cic_init() != 0) return(1);
+  if (!playback && cis_pid == 0 && cic_init() != 0) return(1);
   if (cmdtext == NULL) {
 	cic_msg_type = CMDINTERP_QUIT;
 	sparts = 2;
+	clen = 0;
+	nl_error(-3, "Sending Quit to Server");
   } else {
-	cic_msg_type = test ? CMDINTERP_TEST : CMDINTERP_SEND;
-	clen = strlen(cmdtext) + 1;
+	switch (mode) {
+	  case 1: cic_msg_type = CMDINTERP_TEST; break;
+	  case 2: cic_msg_type = CMDINTERP_SEND_QUIET; break;
+	  default: cic_msg_type = CMDINTERP_SEND; break;
+	}
+	clen = strlen(cmdtext);
+	{ int len = clen;
+	  if (len > 0 && cmdtext[len-1]=='\n') len--;
+	  nl_error(-3, "%*.*s", len, len, cmdtext);
+	}
+	clen++;
 	if (clen > CMD_INTERP_MAX) {
 	  if (nl_response)
 		nl_error(nl_response, "Command too long: %s", cmdtext);
@@ -141,6 +162,7 @@ int ci_sendcmd(const char *cmdtext, int test) {
 	_setmx(&cic_msgs[2], cmdtext, clen);
 	sparts = 3;
   }
+  if (playback) return(0);
   rv = Sendmx(cis_pid, sparts, 1, cic_msgs, &cic_reply);
   if (rv == -1) {
 	switch (errno) {
@@ -152,7 +174,7 @@ int ci_sendcmd(const char *cmdtext, int test) {
 		break;
 	  case ESRCH:
 		if (nl_response)
-		  nl_error(nl_response, "Cmd Interpreter Terminated");
+		  nl_error(nl_response, "Cmd Server Terminated");
 		cis_pid = 0;
 		break;
 	}
@@ -163,13 +185,17 @@ int ci_sendcmd(const char *cmdtext, int test) {
 	if (cic_reply_code == CMDREP_QUIT) cis_pid = 0;
 	else if (cic_reply_code >= CMDREP_EXECERR) {
 	  if (nl_response)
-		nl_error(1, "Execution error %d from Cmd Interpreter",
+		nl_error(1, "Execution error %d from Cmd Server",
 				cic_reply_code - CMDREP_EXECERR);
 	} else if (cic_reply_code >= CMDREP_SYNERR) {
-	  if (nl_response)
-		nl_error(nl_response, "CMDREP_SYNERR: char %d in \"%s\"",
-				cic_reply_code - CMDREP_SYNERR, cmdtext);
-	} else nl_error(4, "Unexpected reply %d from Cmd Interpreter",
+	  if (nl_response && clen) {
+		clen--;
+		if (clen > 0 && cmdtext[clen-1]=='\n') clen--;
+		nl_error(2, "Syntax Error in ci_sendcmd:");
+		nl_error(2, "%*.*s", clen, clen, cmdtext);
+		nl_error(2, "%*s", cic_reply_code - CMDREP_SYNERR, "^");
+	  }
+	} else nl_error(4, "Unexpected reply %d from Cmd Server",
 				cic_reply_code);
   }
   return(cic_reply_code);
