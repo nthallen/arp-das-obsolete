027e545c361a41c044a1c3084b926efe17aa2f5e
Norton Allen <allen@huarp.harvard.edu>
1994-02-16 02:07:25 +0000

    *** empty log message ***

--- QNX4/libsrc/libmaint.mk
+++ QNX4/libsrc/libmaint.mk
@@ -7,22 +7,36 @@
 #   Makelib clean|tidy
 #     Invokes maint.mk2 for each model.
 # $Log$
+# Revision 1.1  1993/02/09  14:58:12  eil
+# Initial revision
+#
 every :
-	@if test -z "$(MODELS)"; then echo MODELS not defined; exit 1; fi; :
+	@if [ -z "$(MODELS)" ]; then\
+	  echo MODELS not defined >&2;\
+	  exit 1;\
+	fi; :
 	@for i in $(MODELS); do\
-	  if test ! -d $$i; then\
-		echo Making directory for model $$i;\
-		mkdir -m g+w $$i;\
+	  if [ $$i != 3r -o -n "$(WCC32)" ]; then\
+		if [ ! -d $${i}$(WCC32) ]; then\
+		  echo Making directory for model $${i}$(WCC32);\
+		  mkdir -m g+w $${i}$(WCC32);\
+		fi;\
+		echo Making MODEL $${i}$(WCC32);\
+		if [ $$i = 3r ]; then\
+		  $(MAKE) MODEL=$$i MODELARGS="-ms -3";\
+		else\
+		  $(MAKE) MODEL=$$i;\
+		fi;\
 	  fi;\
-	  echo Making MODEL $$i;\
-	  $(MAKE) MODEL=$$i;\
 	done; :
 clean tidy :
 	@if test -z "$(MODELS)"; then echo MODELS not defined; exit 1; fi; :
 	@for i in $(MODELS); do\
-	  if test ! -d $$i; then\
-		echo Directory for model $$i does not exist;\
-	  else Make MODEL=$$i $@;\
+	  if [ $$i != 3r -o -n "$(WCC32)" ]; then\
+		if test ! -d $$i; then\
+		  echo Directory for model $$i does not exist;\
+		else Make MODEL=$$i $@;\
+		fi;\
 	  fi;\
 	done; :
 include Makefile
