cd7b3c7be0ce003b403f844170165c662dcdb647
Eileen Schomp <eil@huarp.harvard.edu>
1994-02-16 15:02:57 +0000

    with debug msgs

--- QNX4/eilsrc/das/mods/nav/nav.c
+++ QNX4/eilsrc/das/mods/nav/nav.c
@@ -15,7 +15,6 @@
 #include <fcntl.h>
 #include <time.h>
 #include <sys/timers.h>
-#include <sys/name.h>
 #include <sys/kernel.h>
 #include <sys/timers.h>
 #include <sys/name.h>
@@ -30,13 +29,13 @@
 #define NAV HDR
 /* a timeout flag, seconds and a default, valid when C flag given */
 /* a quit flag, valid when C flag given, quit after time set or timeout */
-#define OPT_MINE "CdT:qQ"
+#define OPT_MINE "CU:qQ"
 #define FRAME_CHAR 0xAA
-#define TIMEOUT 10
+#define NAV_TIMEOUT 10
 #define MAKEWORD(X) ( (buf[X] << 8) | buf[X+1] )
 
 /* global variables */
-char *opt_string=OPT_MSG_INIT OPT_MINE OPT_BREAK_INIT OPT_CC_INIT OPT_SERIAL_INIT;
+char *opt_string=OPT_MSG_INIT OPT_MINE OPT_CC_INIT OPT_SERIAL_INIT;
 short nav_params[5];
 int terminated;
 int timer_signal;
@@ -50,12 +49,8 @@ terminated = 1;
 
 void tim_signalfunction(int sig_number) {
 timer_signal = 1;
-if (set_clock == 1) set_clock = 0;
+if (set_clock > 0) set_clock = 0;
 if (quitter) terminated = 1;
-/* is the following proper in a signal handler for this program ? */
-/* else if ((qnx_name_attach(getnid(),LOCAL_SYMNAME(NAV,name)))==-1)
-    msg(MSG_EXIT_ABNORM,"Can't attach name %s",name);
-*/
 }
 
 void main(int argc, char **argv) {
@@ -65,12 +60,10 @@ extern char *optarg;
 extern int optind, opterr, optopt;
 
 /* local variables */
-int i, fr;
+int i,j,fr;
 int msg_big_ctrl, msg_lit_ctrl;
 unsigned char buf[28];
-char name[NAME_MAX];
 int fd;
-int debug;
 timer_t timer_id;
 struct timespec tp;
 struct tm *timestruct;
@@ -88,19 +81,18 @@ long timeout;
 int name_attached;
 int with_dg;
 int quit_no_dg;
+int jd;
 
     /* initialise msg options from command line */
     signal(SIGQUIT,my_signalfunction);
     msg_init_options(HDR,argc,argv);
     BEGIN_MSG;
-    break_init_options(argc,argv);
 
     /* initialisations */
     first_frame = 1;
     terminated = 0;
     timer_signal = 0;
     set_clock = 0;
-    debug = 0;
     msg_big_ctrl = 0;
     msg_lit_ctrl = 0;
     quitter = 0;
@@ -108,6 +100,7 @@ int quit_no_dg;
     name_attached = 0;
     with_dg = 0;
     quit_no_dg = 0;
+    jd = -1;
 
     /* process args */
     opterr = 0;
@@ -118,12 +111,11 @@ int quit_no_dg;
 	    case 'C':
 		    if (seteuid(0) == -1)
 			msg(MSG_WARN,"Can't set euid to root");
-		    set_clock = 1;
+		    set_clock++;
 		    break;
-	    case 'd': debug = 1; break;
 	    case 'q': quitter = 1; break;
 	    case 'Q': quit_no_dg = 1; break;
-	    case 'T': timeout = atol(optarg); break;
+	    case 'U': timeout = atol(optarg); break;
 	    case '?': msg(MSG_EXIT_ABNORM,"Invalid option -%c",optopt);
 	    default: break;
 	}
@@ -138,7 +130,7 @@ int quit_no_dg;
 	    msg(MSG_WARN,"option T not valid without option C: ignored");
 	    timeout = -1;
 	}
-    } else if (timeout < 0) timeout = TIMEOUT;
+    } else if (timeout < 0) timeout = NAV_TIMEOUT;
 
     if (optind >= argc) msg(MSG_EXIT_ABNORM,"no device specified");
     if (argc > optind+1) msg(MSG_EXIT_ABNORM,"only one device allowed");
@@ -159,9 +151,8 @@ int quit_no_dg;
 	timer.it_interval.tv_nsec = 0L;
     }
 
-    set_response(NLRSP_QUIET);
     /* register with cmdctrl */
-    if (!debug) cc_init_options(argc,argv,0,0,0,0,QUIT_ON_QUIT);
+    cc_init_options(argc,argv,0,0,0,0,QUIT_ON_QUIT);
 
     fr = 1;
     buf[0] = !(FRAME_CHAR);
@@ -172,6 +163,8 @@ int quit_no_dg;
 	reltimer( timer_id,  &timer, NULL);
     }
 
+    set_response(NLRSP_WARN); /* for norts Col_set_pointer */
+
     while (!terminated) {
 
 	while (fr)
@@ -181,8 +174,8 @@ int quit_no_dg;
 		if (terminated) break;
 		/* signal SIGUSR */
 		else if (timer_signal && !name_attached) {
-		    if ((qnx_name_attach(getnid(),LOCAL_SYMNAME(NAV,name)))==-1)
-			msg(MSG_EXIT_ABNORM,"Can't attach name %s",name);
+		    if (qnx_name_attach(getnid(),LOCAL_SYMNAME(NAV))==-1)
+			msg(MSG_EXIT_ABNORM,"Can't attach symbolic name for %s",NAV);
 		    name_attached = 1;
 		    continue;
 		}
@@ -196,8 +189,8 @@ int quit_no_dg;
 	i = fr ? 26 : 28;
 	if (terminated) break;
 	else if (timer_signal && !name_attached) {
-	    if ((qnx_name_attach(getnid(),LOCAL_SYMNAME(NAV,name)))==-1)
-		msg(MSG_EXIT_ABNORM,"Can't attach name %s",name);
+	    if (qnx_name_attach(getnid(),LOCAL_SYMNAME(NAV))==-1)
+		msg(MSG_EXIT_ABNORM,"Can't attach symbolic name for %s",NAV);
 	    name_attached = 1;
 	}
 	
@@ -205,8 +198,8 @@ int quit_no_dg;
 	while (dev_read(fd, fr ? &buf[2] : buf, i, i, 0, 0, 0, 0) == -1)
 	    if (terminated) break;
 	    else if (timer_signal && !name_attached) {
-		if ((qnx_name_attach(getnid(),LOCAL_SYMNAME(NAV,name)))==-1)
-		    msg(MSG_EXIT_ABNORM,"Can't attach name %s",name);
+		if (qnx_name_attach(getnid(),LOCAL_SYMNAME(NAV))==-1)
+		    msg(MSG_EXIT_ABNORM,"Can't attach symbolic name for %s",NAV);
 		name_attached = 1;
 		continue;
 	    }
@@ -222,8 +215,8 @@ int quit_no_dg;
 	}
 
 	if (timer_signal && !name_attached) {
-	    if ((qnx_name_attach(getnid(),LOCAL_SYMNAME(NAV,name)))==-1)
-		msg(MSG_EXIT_ABNORM,"Can't attach name %s",name);
+	    if (qnx_name_attach(getnid(),LOCAL_SYMNAME(NAV))==-1)
+		msg(MSG_EXIT_ABNORM,"Can't attach symbolic name for %s",NAV);
 	    name_attached = 1;
 	}
 
@@ -248,7 +241,8 @@ int quit_no_dg;
 	    if (buf[2] & 0x10) 
 		msg(MSG,"Time Source: G.O.E.S. time code generator");
 	    else msg(MSG,"Time Source: Unverified Internal Clock");
-	    msg(MSG,"Julian Date: %d%d%d",buf[2] & 0x03, buf[3] >> 4, buf[3] & 0x0F);
+	    jd = (buf[2] & 0x03) * 100 + (buf[3] >> 4) * 10 + (buf[3] & 0x0F);
+	    msg(MSG,"Julian Date: %d",jd);
 	    first_frame = 0;
 	}
 
@@ -263,11 +257,28 @@ int quit_no_dg;
 	if (terminated) break;
 
 	/* set system clock on first frame */
-	if (set_clock == 1) {
+	if (set_clock > 0) {
 	    /* substitute in hours and seconds */
 	    timestruct->tm_hour = nav_hrs;
 	    timestruct->tm_min = nav_mins;
 	    timestruct->tm_sec = nav_secs;
+	    if (set_clock > 1) {
+		/* figure day of month and months since Jan from Julian day */
+		for (i=0;i<12;i++) {
+		    j = jd;
+		    switch (i) {
+			case 1: /* feb */
+			    if ( !((timestruct->tm_year+1900) % 4)) jd -= 29;
+			    else jd -= 28;
+			    break;
+			case 3: case 5: case 8: case 10: jd -= 30; break;
+			default: jd -= 31; break;
+		    }
+		    if (jd <= 0) break;
+		}
+		timestruct->tm_mon = i;
+		timestruct->tm_mday = j;
+	    }
 	    /* set system clock */
 	    tp.tv_sec = mktime(timestruct);
 	    tp.tv_nsec = 0;
@@ -276,15 +287,15 @@ int quit_no_dg;
 	    if ( (setclock(TIMEOFDAY,&tp)) == -1)
 		msg(MSG_WARN,"Can't set clock");
 	    else {
-		msg(MSG,"setting system clock to %.24s %s %s",ctime(&tp.tv_sec),tzname[0],tzname[1]);
+		msg(MSG,"setting system clock to %.24s %s",ctime(&tp.tv_sec),localtime(&tp.tv_sec)->tm_isdst ? tzname[1] : tzname[0]);
 		/* get current system time */
 		sys_secs_from_mid = nav_secs_from_mid;
 	    }
-	    set_clock = 2;
+	    set_clock = -1;
 	    if (quitter) terminated = 1;
 	    else  /* register yourself */
-		if ((qnx_name_attach(getnid(),LOCAL_SYMNAME(NAV,name)))==-1)
-		    msg(MSG_WARN,"Can't attach name %s",name);
+		if (qnx_name_attach(getnid(),LOCAL_SYMNAME(NAV))==-1)
+		    msg(MSG_WARN,"Can't attach symbolic name for %s",NAV);
 	}
 
 	temp = nav_secs_from_mid - sys_secs_from_mid;
@@ -305,29 +316,33 @@ int quit_no_dg;
 	    temp %= SHRT_MIN;
 	}
 
-	if (!debug && !with_dg)
+	if (!with_dg)
 	    if (!(Col_set_pointer(NAV_FLAG_ID, nav_params, 0))) {
 		with_dg = 1;
 		msg(MSG,"achieved cooperation with DG");
-	    } else if (quit_no_dg)
+	    } else
+		if (quit_no_dg)
 		    msg(MSG_EXIT_ABNORM,"Can't cooperate with DG");
-		else errno = 0;
+		else {
+		    errno = 0;
+		    set_response(NLRSP_QUIET); /* for norts Col_set_pointer */
+		}
 
 	wd = temp;
 	/* form words and update array */
-	if (debug) msg(MSG,"time deviation: %d",wd);
+	msg(MSG_DEBUG,"time deviation: %d",wd);
 	nav_params[0] = wd;
 	wd = MAKEWORD(6); /* lattitude */
-	if (debug) msg(MSG,"lattitude: %d",wd);
+	msg(MSG_DEBUG,"lattitude: %d",wd);
 	nav_params[1] = wd;
 	wd = MAKEWORD(8); /* longitude */
-	if (debug) msg(MSG,"longitude: %d",wd);
+	msg(MSG_DEBUG,"longitude: %d",wd);
 	nav_params[2] = wd;
 	wd = MAKEWORD(20); /* altitude */
-	if (debug) msg(MSG,"altitude: %d",wd);
+	msg(MSG_DEBUG,"altitude: %d",wd);
 	nav_params[4] = wd;
 	wd = MAKEWORD(22); /* true air speed */
-	if (debug) msg(MSG,"true air speed: %u",wd);
+	msg(MSG_DEBUG,"true air speed: %u",wd);
 	nav_params[3] = wd;
 
     } /* while */
