7b90bc944520e1ace41b371772eea0168360ccbc
Norton Allen <allen@huarp.harvard.edu>
1994-02-16 01:53:49 +0000

    Details

--- QNX4/libsrc/cltmain.c
+++ QNX4/libsrc/cltmain.c
@@ -1,5 +1,8 @@
 /* cltmain.c A main program for interactive commanding as a client.
  * $Log$
+ * Revision 1.1  1993/05/28  20:06:06  nort
+ * Initial revision
+ *
  * Revision 1.1  1993/03/01  18:40:18  nort
  * Initial revision
  *
@@ -9,7 +12,10 @@
 #include "nl_cons.h"
 #include "msg.h"
 #include "cmdalgo.h"
-static char rcsid[] = "$Id$";
+#pragma off (unreferenced)
+  static char rcsid[] =
+	"$Id$";
+#pragma on (unreferenced)
 
 char *opt_string = OPT_MSG_INIT OPT_CON_INIT OPT_CIC_INIT;
 int (*nl_error)(unsigned int level, char *s, ...) = msg;
@@ -19,6 +25,8 @@ int main(int argc, char **argv) {
   Con_init_options(argc, argv);
   cic_options(argc, argv, "cic");
   atexit(nlcon_close);
+  BEGIN_MSG;
   cmd_interact();
+  DONE_MSG;
   return(0);
 }
--- QNX4/libsrc/srvmain.c
+++ QNX4/libsrc/srvmain.c
@@ -1,21 +1,33 @@
 /* srvmain.c A main program for command servers.
  * $Log$
+ * Revision 1.1  1993/05/28  20:06:09  nort
+ * Initial revision
+ *
  * Revision 1.1  1993/03/01  18:40:26  nort
  * Initial revision
  *
  */
+#include <sys/types.h>
+#include <unistd.h>
 #include "nortlib.h"
 #include "msg.h"
 #include "cmdalgo.h"
 #include "subbus.h"
-static char rcsid[] = "$Id$";
+#pragma off (unreferenced)
+  static char rcsid[] =
+	"$Id$";
+#pragma on (unreferenced)
 
 char *opt_string = OPT_MSG_INIT;
 int (*nl_error)(unsigned int level, char *s, ...) = msg;
+void cis_init(void);
 
 int main(int argc, char **argv) {
+  seteuid(getuid());
   msg_init_options("Cmd", argc, argv);
+  BEGIN_MSG;
   load_subbus();
   ci_server();
+  DONE_MSG;
   return(0);
 }
