a8db4d524e2748813c74fbf11fad1572d7a9713d
Eileen Schomp <eil@huarp.harvard.edu>
1994-07-19 14:37:54 +0000

    the memo in use

--- QNX4/memo/memo.c
+++ QNX4/memo/memo.c
@@ -14,6 +14,7 @@
 #include <sys/kernel.h>
 #include <sys/name.h>
 #include <sys/psinfo.h>
+#include <sys/types.h>
 #include <memo.h>
 #include <reply.h>
 #include <eillib.h>
@@ -23,7 +24,7 @@
 #define OPT_MINE "k:q"
 
 /* globals */
-char *opt_string=OPT_MSG_INIT OPT_BREAK_INIT OPT_MINE;
+char *opt_string=OPT_MSG_INIT OPT_MINE;
 
 main(int argc, char **argv) {
 
@@ -32,17 +33,16 @@ extern char *optarg;
 extern int optind, opterr, optopt;
 
 char recv_msg[MEMO_MSG_MAX];
-char name[NAME_MAX];
 pid_t who;
 nid_t n;
 int i;
 reply_type rv;
-int audio_rec = 0;
+int aud = 0;
+int got_quit = 0;
 
     /* initialise das options from command line */    
     msg_init_options(HDR,argc,argv);
     BEGIN_MSG;
-    break_init_options(argc,argv);
 
     /* process args */
     opterr = 0;
@@ -50,26 +50,28 @@ int audio_rec = 0;
     do {
 	i=getopt(argc,argv,opt_string);
 	switch (i) {
-	    case 'q': audio_rec = 1; break;
+	    case 'q': aud = 1; break;
  	    case 'k':
 		n = atoi(optarg);
 		rv = MEMO_DEATH_HDR;
-		if ( (who = qnx_name_locate(n,LOCAL_SYMNAME(MEMO,name),0,0))!=-1) {
+		if ( (who = qnx_name_locate(n,LOCAL_SYMNAME(MEMO),0,0))!=-1) {
 		    if (Send(who,&rv,&rv,sizeof(reply_type),sizeof(reply_type))==-1)
 			msg(MSG_EXIT_ABNORM,"error sending to %s: task %d",MEMO,who);
 		    if (rv!=REP_OK)
 			msg(MSG_EXIT_ABNORM,"bad response from %s: task %d",MEMO,who);
 		}
-		else msg(MSG_EXIT_ABNORM,"Can't find %s on node %d",name,n);
-		DONE_MSG;
+		else msg(MSG_EXIT_ABNORM,"Can't find %s on node %d",MEMO,n);
+		msg(MSG,"task %d: completed",getpid());
+		msg_end();
+		exit(0);
 	    case '?': msg(MSG_EXIT_ABNORM,"Invalid option -%c",optopt);
 	    default: break;
 	}
     } while (i!=-1);
 
     /* register yourself */
-    if ((qnx_name_attach(getnid(),LOCAL_SYMNAME(MEMO,name)))==-1)
-	msg(MSG_EXIT_ABNORM,"Can't attach name %s",name);
+    if ((qnx_name_attach(getnid(),LOCAL_SYMNAME(MEMO)))==-1)
+	msg(MSG_EXIT_ABNORM,"Can't attach symbolic name for %s",MEMO);
 
     /* sort messages by priority */
     qnx_pflags(~0,_PPF_PRIORITY_REC,0,0);
@@ -77,31 +79,43 @@ int audio_rec = 0;
     while(1) {
 	rv = REP_OK;
 	errno=0;
-	if ((who=Receive(0,recv_msg,MEMO_MSG_MAX))==-1)
-	    msg(MSG_WARN,"error on receive");	
-	else {
-	    switch( recv_msg[0] ) {
-		case MEMO_HDR:
-		    if (Reply(who, &rv, sizeof(reply_type))==-1)
-			msg(MSG_WARN,"error replying to task %d",who);
-		    i=MSG;
-		    if (audio_rec)
-			if (strstr(recv_msg,FATAL_STR) || strstr(recv_msg,FAIL_STR))
-			    i=MSG_FAIL;
-			else if (strstr(recv_msg,WARN_STR)) i=MSG_WARN;
-		    msg(i,"%s",recv_msg+1);
-		    break;
-		case MEMO_DEATH_HDR:
-		    if (Reply(who, &rv, sizeof(reply_type))==-1)
-			msg(MSG_WARN,"error replying to task %d",who);
-		    msg_end();
-		    DONE_MSG;
-		    exit(0);
-		default: rv = REP_UNKN;
-		    msg(MSG_WARN,"unrecognised msg received");
-		    if (Reply(who, &rv, sizeof(reply_type))==-1)
-			msg(MSG_WARN,"error replying UNKNOWN to task %d",who);
+
+	if (!got_quit)
+	    while ((who=Receive(0,recv_msg,MEMO_MSG_MAX))==-1)
+		msg(MSG_WARN,"error on receive");	
+	else
+	    if ((who=Creceive(0,recv_msg,MEMO_MSG_MAX))==-1) {
+		msg(MSG,"message queue empty and quit received");
+		msg(MSG,"task %d: completed",getpid());
+		msg_end();
+		exit(0);
 	    }
-	}
+
+	switch (recv_msg[0]) {
+	    case MEMO_HDR:
+		if (Reply(who, &rv, sizeof(reply_type))==-1)
+		    msg(MSG_WARN,"error replying to task %d",who);
+		i=MSG;
+		if (aud==1)
+		    if (strstr(recv_msg,FATAL_STR) || strstr(recv_msg,FAIL_STR))
+			i=MSG_FAIL;
+		    else if (strstr(recv_msg,WARN_STR)) 
+			i=MSG_WARN;
+		    else if (strstr(recv_msg,DEBUG_STR)) 
+			i=MSG_DEBUG;
+		msg(i,"%s",recv_msg+1);
+		break;
+	    case MEMO_DEATH_HDR:
+		if (Reply(who, &rv, sizeof(reply_type))==-1)
+		    msg(MSG_WARN,"error replying to task %d",who);
+		got_quit = 1;
+		break;
+	    default:
+		rv = REP_UNKN;
+		msg(MSG_WARN,"unrecognised msg received");
+		if (Reply(who, &rv, sizeof(reply_type))==-1)
+		    msg(MSG_WARN,"error replying UNKNOWN to task %d",who);
+	    }
+
     } /* while */
 }
