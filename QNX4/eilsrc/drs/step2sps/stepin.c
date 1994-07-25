2531482aced111742c21203313f0d650acaf38ff
Eileen Schomp <eil@huarp.harvard.edu>
1994-07-25 18:29:14 +0000

    added support for format 2010

--- QNX4/eilsrc/drs/step2sps/stepin.c
+++ QNX4/eilsrc/drs/step2sps/stepin.c
@@ -8,6 +8,7 @@
    Made ANSI on or after October 17, 1989
    Modified by Eil, July 1991 for Ascii File Format Standards Document
    Version 1.1, reads FFI = 1001, 1010, 1020.
+   Version 1.3: also reads FFI = 2010.
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -28,28 +29,17 @@
 extern int no_aux_ask;
 extern int not_gmt;
 extern int nmon;
-int auxing;
-
+int auxing=0;
 
 int set_scale_miss_name(FILE *fp, sps_ptr ss, int nv, double *sf, double *mv);
 
-/*+ DATA REDUCTION
-<sort> read_vals
-
-int
-read_vals(fp, nv, vals)
-FILE *fp;
-int nv;
-double *vals;
-
--*/
 int read_vals(FILE *fp, int nv, double *vals) {
   int i;
   char buf[LN];
 
   for (i = 0; i < nv; i++) {
 	if (fscanf(fp," %lf",vals) != 1) {
-	      cmderr("Bad scale factor or missing value");
+	      cmderr("error reading value");
 	      return(1);
 	  }
     vals++;
@@ -64,25 +54,16 @@ int ffi;
 char ass_name[LN]={'\0'};
 sps_ptr ass;
 
-
-/*+ DATA REDUCTION
-<sort> read_hdr
-
-sps_ptr read_hdr(FILE *fp, char *ss_name, int *nv, int *nsph, int *nspl,
-             double *bd, double *dt, double *sf, double *mv,
-			int *nauxv, double *asf, double *amv);
-
--*/
-sps_ptr read_hdr(FILE *fp, char *ss_name, int *nv, int *nsph, int *nspl,
-             double *bd, double *dt, double *sf, double *mv,
-	                  int *nauxv, double *asf, double *amv) {
+int read_hdr(FILE *fp, char *ss_name, int *niv, int *nv, int *nsph,
+            double *bd, double *dx, double *sf, double *mv,
+            int *nauxv, double *asf, double *amv, int *nx, int *nxdef,
+			int *nxwhich) {
   int nlhdr, i, yr, mo, dy;
   sps_ptr ss;
   char buf[LN];
   struct tm timeptr={0};
 
   *nauxv=0;
-  auxing=0;
   /* read number of lines in header and ffi index */
   read_line(fp, buf);
   if (sscanf(buf, "%d%d", &nlhdr, &ffi) != 2 || nlhdr <= 0) {
@@ -90,9 +71,14 @@ sps_ptr read_hdr(FILE *fp, char *ss_name, int *nv, int *nsph, int *nspl,
     return(-1);
   }
 
-  if (ffi != 1001 && ffi !=1020 && ffi != 1010) {
-    cmderr("Only FFI 1001, 1010 and 1020 Formats accepted");
-    return(-1);
+  switch (ffi) {
+  	case 1001:
+  	case 1020:
+  	case 1010: *niv=1; break;
+  	case 2010: *niv=2; break;
+  	default:
+	    cmderr("Only FFI 1001, 1010 and 1020 Formats accepted");
+	    return(-1);  	
   }
 
   /* line 2 - Experimenter ; ONAME */
@@ -129,14 +115,38 @@ sps_ptr read_hdr(FILE *fp, char *ss_name, int *nv, int *nsph, int *nspl,
   	*bd = *bd - timezone;
   }
  
-  /* line 8  DX(1) */
+  /* line 8  DX */
   read_line(fp, buf);   /* line 8 */
   buf[69] = '\0';
   dr_calc_cmd_line(buf, NULL);
-  if (sscanf(buf, "%lf", dt) != 1 || *dt < 0) {
+  if (sscanf(buf, "%lf %lf %lf %lf", &dx[0], &dx[1], &dx[2], &dx[3]) != *niv
+		|| dx[0] < 0 || dx[1] < 0 || dx[2] < 0 || dx[3] < 0) {
     cmderr("Error reading time interval (line 8)");
     return(-1);
   }
+
+  /* NX, NXDEF, X */
+  if (ffi == 2010) {
+    read_line(fp, buf);
+	buf[69] = '\0';
+	dr_calc_cmd_line(buf, NULL);
+  	if (sscanf(buf, "%d", nx) != 1 || *nx <= 0) {
+  		cmderr("Error reading number of values for bounded independent var");
+  		return(-1);
+  	}
+    read_line(fp, buf);
+	buf[69] = '\0';
+	dr_calc_cmd_line(buf, NULL);
+  	if (sscanf(buf, "%d", nxdef) != 1) {
+  		cmderr("Error reading header number of values for bounded independent var");
+  		return(-1);
+  	}
+  	for (i=0;i<*nxdef;i++) fscanf(fp,"%*lf");
+    swlw_ln(fp);  	
+	buf[0]='\0';
+	dr_calc_cmd_io("Which constant bounded independent variable value (1,2..)? [1] ",buf,1);
+	if (sscanf(buf,"%d",nxwhich)!=1) cmderr("Error reading which independent value to use");
+  } else *nx = 1, *nxwhich = 1, *nxdef = 0;
 
   /* line 9, depends on ffi */
   if (ffi == 1020) {
@@ -150,7 +160,8 @@ sps_ptr read_hdr(FILE *fp, char *ss_name, int *nv, int *nsph, int *nspl,
   } else *nsph = 1;
 
   /* next line XNAME */
-  swlw_ln(fp);
+  for (i=0;i<*niv;i++)
+	  swlw_ln(fp);
 
   /* next line NV */
   read_line(fp, buf);
@@ -160,7 +171,6 @@ sps_ptr read_hdr(FILE *fp, char *ss_name, int *nv, int *nsph, int *nspl,
     cmderr("Error reading number of variables NV");
     return(-1);
   }
-
   if (ss_error(ss = ss_create(ss_name, nmon ? SPT_NON_MONOTONIC : SPT_INCREASING, *nv+1, 1))) {
     cmderr("Cannot open spreadsheet %s", ss_name);
     return(-1);
@@ -176,21 +186,11 @@ sps_ptr read_hdr(FILE *fp, char *ss_name, int *nv, int *nsph, int *nspl,
     return(-1);
   }
 
-
-/*  read_line(fp, buf);
-  if (sscanf(buf, "%d", nspl) != 1 || *nspl <= 0) {
-    cmderr("Error reading samples per line (line 10)");
-    return(-1);
-  }
-*/
-  *nspl = *nsph;
-
   /* read scale factors and miss values and names */
-  if (set_scale_miss_name(fp, ss, *nv, sf, mv)) return(-1);
+  if (set_scale_miss_name(fp, ss, *nv, sf, mv)) return(-1);  
 
-
-  /* if ffi = 1010, 1020 read number auxillary variables */
-  if (ffi == 1020 || ffi == 1010) {
+  /* read number auxillary variables */
+  if (ffi == 1020 || ffi == 1010 || ffi == 2010) {
   	read_line(fp, buf);
   	buf[69] = '\0';
   	dr_calc_cmd_line(buf, NULL);
@@ -204,9 +204,9 @@ sps_ptr read_hdr(FILE *fp, char *ss_name, int *nv, int *nsph, int *nspl,
 		dr_calc_cmd_io("Create auxillary variable spreadsheet also? [n] ",buf,1);
 		if (!strchr(strlwr(buf),'y')) auxing=0;
 		else auxing=1;
-	}
+	}  	
   }
-
+  
   if (auxing) {
 	if (dr_calc_cmd_io("Auxillary Spreadsheet Name [auxly] ",ass_name,1))
 		strcpy(ass_name,"auxly");
@@ -223,19 +223,11 @@ sps_ptr read_hdr(FILE *fp, char *ss_name, int *nv, int *nsph, int *nspl,
   		ss_close(ass);
   		return(-1);
   	}
-
-
+    if (set_scale_miss_name(fp, ass, *nauxv, asf, amv)) return(-1);  	
   }
-
-  if (ffi == 1020 || ffi == 1010) {
-  	/* read scale factors and miss values and names */
-	if (auxing) {
-  	   if (set_scale_miss_name(fp, ass, *nauxv, asf, amv)) return(-1);
-	}
-	else if (*nauxv)
-	   for (i=0;i<*nauxv+2;i++)
-  	       swlw_ln(fp);
-  }
+  else if (*nauxv)
+    for (i=0;i<*nauxv+2;i++)
+       swlw_ln(fp);
 
   /* read special and normal comments */
   for (i=0;i<2; i++) {
@@ -247,7 +239,6 @@ sps_ptr read_hdr(FILE *fp, char *ss_name, int *nv, int *nsph, int *nspl,
   	for (yr=0; yr<dy; yr++) swlw_ln(fp);
   }
 
-/*  if (!auxing) *nauxv=0;*/
   return(ss);
 }
 
@@ -280,18 +271,6 @@ return(0);
 }
 
 
-
-
-/*+ DATA REDUCTION
-<sort> read_value
-int
-read_value(fp, ss, i, T, sf, mv, missed, delta, bd)
-FILE *fp;
-sps_ptr ss;
-int i, *missed;
-double T, *sf, *mv, delta, bd;
-
--*/
 int read_value(FILE *fp, sps_ptr ss, int i, double T, double *sf,
                double *mv, int *missed, double delta, double bd) {
   double V;
@@ -326,57 +305,56 @@ int read_value(FILE *fp, sps_ptr ss, int i, double T, double *sf,
   return(0);
 }
 
-/*+ DATA REDUCTION
-<sort> stepin
-
-void
-stepin(filename, ss_name)
-char *filename, *ss_name;
--*/
 void stepin(char *filename, char *ss_name) {
   FILE *fp;
   sps_ptr ss;
   char *fbuf;
-  int nv, nsph, nspl, nppl, nauxv;
-  int i, j, k, missed[MAX_VARS], amissed[MAX_VARS];
-  double bd, dt, sf[MAX_VARS], mv[MAX_VARS], asf[MAX_VARS], amv[MAX_VARS];
+  int niv=0, nv=0, nsph=0, nauxv=0;
+  int i, j, n, missed[MAX_VARS], amissed[MAX_VARS];
+  double bd, sf[MAX_VARS], mv[MAX_VARS], asf[MAX_VARS], amv[MAX_VARS];
   double hack, T, delta;
-
+  double dx[3] = { 0,0,0 };
+  int nx=1, nxdef=0, nxwhich=1;
+  
   if ((fp = snfopen(filename, "r", &fbuf)) == NULL) {
     cmderr("Cannot open input file %s", filename);
     return;
   }
   set_dr_window("STEP Input", 1, 1);
-  ss = read_hdr(fp, ss_name, &nv, &nsph, &nspl, &bd, &dt, sf, mv, &nauxv, asf, amv);
-/*  dt /= nsph; WRONG! */
+  ss = read_hdr(fp, ss_name, &niv, &nv, &nsph, &bd, dx, sf, mv,
+		 &nauxv, asf, amv, &nx, &nxdef, &nxwhich);
+
   if (ss_error(ss)) {
     snfclose(fp, fbuf);
     return;
   }
   sdoupdate(0);
-  nppl = nspl*nv;
+  
   /* <read in the data> */
-  delta = dt/2.;
+  delta = dx[niv-1]/2.;
   for (i = 0; i < nv; i++) missed[i] = 1;
   for (i = 0; i < nauxv; i++) amissed[i] = 1;
   for (;;) {
+  	/* read independent var: time */
     if (fscanf(fp, "%lf", &hack) != 1) break;
     hack += bd;
-/*    if (nsph > 1)  { */
-	if (nauxv)
-	  if (auxing) {
-		  for (i = 0; i < nauxv; i++)
-			if (read_value(fp, ass, i, hack, asf, amv, amissed, (dt*nsph)/2, bd)) break;
-	  }
+    if (nauxv)
+       if (auxing) {
+   	      for (i = 0; i < nauxv; i++)
+			/* read auxillary vars */   	      
+            if (read_value(fp, ass, i, hack, asf, amv, amissed, (dx[niv-1]*nsph)/2, bd)) break;
+       }
 	  else  swlw_ln(fp);
-/*    } */
-    for (T = hack, j = 0; j < nsph; j++, T += dt) {
-      for (i = 0, k = 0; i < nv; i++) {
-        if (read_value(fp, ss, i, T, sf, mv, missed, delta, bd)) break;
-        if (++k >= nppl) {
-          swlw_ln(fp);
-          k = 0;
+   	/* read primary vars */
+    for (T = hack, j = 0; j < nsph; j++, T += dx[niv-1]) {
+      for (i = 0; i < nv; i++) {
+        for (n = 1; n <= nx; n++) {
+	      	if (nxwhich == n) {
+               if (read_value(fp, ss, i, T, sf, mv, missed, delta, bd)) break;
+		    }
+		    else fscanf(fp,"%*lf");
         }
+        if (n <= nx) break;
       }
       if (i < nv) break;
     }
@@ -388,7 +366,3 @@ void stepin(char *filename, char *ss_name) {
   snfclose(fp, fbuf);
   clear_dr_window();
 }
-
-
-
-
