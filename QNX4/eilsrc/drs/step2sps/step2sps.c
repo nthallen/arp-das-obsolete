5df2e337dc3ea9e78d1a692edb33e7f1dc87ba91
Eileen Schomp <eil@huarp.harvard.edu>
1994-11-07 19:21:44 +0000

    more argument handling

--- QNX4/eilsrc/drs/step2sps/step2sps.c
+++ QNX4/eilsrc/drs/step2sps/step2sps.c
@@ -12,15 +12,34 @@ void end_dr_disp(void);
 int no_aux_ask = 0;
 int not_gmt = 0;
 int nmon = 0;
+int nxwhich = 0;
+
+#define USE_MSG \
+ {\
+ fprintf(stderr, "Command is STEP2SPS  [-a] [-g] [-m] [-i<ind var>] filename ssname\n");\
+ fprintf(stderr, "  -a    don't ask about auxillary variables if they exist\n");\
+ fprintf(stderr, "  -g    don't assume GMT time\n");\
+ fprintf(stderr, "  -m    independent variable not monitonically increasing\n");\
+ fprintf(stderr, "  -i <independent var 1,2,...>	used for format 2010\n");\
+ exit(-1);\
+ }
 
 void main(int argc, char **argv) {
-  if (argc < 3) {
-    fprintf(stderr, "Command is STEP2SPS  [-a] [-g] [-m] filename  ssname\n");
-    fprintf(stderr, "  -a    don't ask about auxillary variables if they exist\n");
-    fprintf(stderr, "  -g    don't assume GMT time\n");
-    fprintf(stderr, "  -m    independent variable not monitonically increasing\n");    
-    exit(-1);
-  }
+int i;
+  if (argc<3) USE_MSG;
+  for (i=1;i<argc;i++)
+  	if (argv[i][0]=='-')
+	  	switch (argv[i][1]) {
+	  		case 'a': no_aux_ask=1; break;
+	  		case 'g': not_gmt=1; break;
+	  		case 'm': nmon=1; break;
+	  		case 'i':
+					if (argv[i][2]=='\0')
+						if (i<argc)
+							nxwhich=atoi(argv[i+1]);
+						else USE_MSG
+					else nxwhich=atoi(argv[i]+2);
+	  	}
 /*  init_attrs(CFG_FILE);
   if (initscr() == ERR) error(-1, "Cannot initscr()"); */
   init_windows();
@@ -29,22 +48,6 @@ void main(int argc, char **argv) {
   nodelay(stdscr, TRUE);
   keypad(stdscr, TRUE); */
   start_dr_disp();
-  /* argument handling is stupid here, but its easier now! */
-  if (argc > 3) {
-	if (!strcmp(argv[1],"-a")) no_aux_ask = 1;
-	else if (!strcmp(argv[1],"-g")) not_gmt = 1;
-	else if (!strcmp(argv[1],"-m")) nmon = 1;  			
-  }
-  if (argc > 4) {
-	if (!strcmp(argv[2],"-a")) no_aux_ask = 1;
-	else if (!strcmp(argv[2],"-g")) not_gmt = 1;
-	else if (!strcmp(argv[2],"-m")) nmon = 1;  		  		
-  }
-  if (argc > 5) {
-	if (!strcmp(argv[3],"-a")) no_aux_ask = 1;
-	else if (!strcmp(argv[3],"-g")) not_gmt = 1;
-	else if (!strcmp(argv[3],"-m")) nmon = 1;  		  		
-  }  
   stepin(argv[argc-2], argv[argc-1]);
   end_dr_disp();
   end_windows();
