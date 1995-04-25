2b84777877bdd0009d3413ff5f41e4f248daf2ee
Eileen Schomp <eil@huarp.harvard.edu>
1995-04-25 15:09:48 +0000

    latest and greatest, bug fixes

--- QNX4/eilsrc/das/mods/sol/soldrv/soldrv.c
+++ QNX4/eilsrc/das/mods/sol/soldrv/soldrv.c
@@ -30,6 +30,7 @@
 #include <subbus.h>
 #include <das.h>
 #include <eillib.h>
+#include <nortlib.h>
 #include <scdc.h>
 #include <soldrv.h>
 #include "sol.h"
@@ -43,6 +44,7 @@ pid_t timer_proxy;
 timer_t timer=0;
 struct itimercb tcb;
 struct itimerspec tval, otval;
+unsigned int stat = 0;
 
 #define DOWN_TIMER { \
     tval.it_value.tv_sec = 0L; \
@@ -57,8 +59,10 @@ struct itimerspec tval, otval;
 	msg(MSG_EXIT_ABNORM, "Error in reltimer")
 #define RESOLUTION_TIMER { \
     /* converts back */ \
-    i=count/16384; \
-    j=(count%16384)/16384; \
+    if (count==0) i = 4; \
+    else i = count/16384; \
+    j = (count%16384) / 16; \
+	j = (j*1953125) / 2; \
     /* set timer resolution */ \
     tval.it_value.tv_sec = (long)i; \
     tval.it_value.tv_nsec = (long)j; \
@@ -112,233 +116,250 @@ unsigned char reg_which;
 
 void main(int argc, char **argv) {
 
-/* getopt variables */
-extern char *optarg;
-extern int optind, opterr, optopt;
+  /* getopt variables */
+  extern char *optarg;
+  extern int optind, opterr, optopt;
 
-/* local variables */
-char name[FILENAME_MAX+1];
-int i, j, exp_ext, ip, count, mode_mode;
-char filename[40];
-reply_type replycode;
-pid_t cmd_tid;
-struct _mxfer_entry smsg[3];
-struct _mxfer_entry rmsg;
-msg_hdr_type hdr_mult = SC_MULTCMD;
-msg_hdr_type hdr_dasc = DASCMD;
-unsigned char type_scdc = DCT_SCDC;
+  /* local variables */
+  char name[FILENAME_MAX+1];
+  int i, exp_ext, ip, mode_mode;
+  long j;
+  unsigned int count;
+  char filename[40];
+  reply_type replycode;
+  pid_t cmd_tid;
+  struct _mxfer_entry smsg[3];
+  struct _mxfer_entry rmsg;
+  msg_hdr_type hdr_mult = SC_MULTCMD;
+  msg_hdr_type hdr_dasc = DASCMD;
+  unsigned char type_scdc = DCT_SCDC;
 
-    /* initialise msg options from command line */
-    msg_init_options(HDR,argc,argv);
-    BEGIN_MSG;
-    signal(SIGQUIT,my_signalfunction);
-    signal(SIGINT,my_signalfunction);
-    signal(SIGTERM,my_signalfunction);
+  /* initialise msg options from command line */
+  msg_init_options(HDR,argc,argv);
+  BEGIN_MSG;
+  signal(SIGQUIT,my_signalfunction);
+  signal(SIGINT,my_signalfunction);
+  signal(SIGTERM,my_signalfunction);
 
-    /* initialisations */
-    mode_request = 0;
-    state = ST_NO_MODE;
-    _setmx(&rmsg,&replycode,sizeof(reply_type));
+  /* initialisations */
+  mode_request = 0;
+  state = ST_NO_MODE;
+  _setmx(&rmsg,&replycode,sizeof(reply_type));
 
-    j=0;
+  j=0;
 #ifdef SYSTEM_BOARD
-    j=1;
+  j=1;
 #endif
 
-    /* process args */
-    opterr = 0;
-    optind = 0;
-    do {
-	i=getopt(argc,argv,opt_string);
-	switch (i) {
-	    case 'q': if (j)
-			msg(MSG,"uses a timer on the Harvard system controller board");
-		      else
-			msg(MSG,"uses a QNX timer");
-		      DONE_MSG;
-		      break;
-	    case '?': msg(MSG_EXIT_ABNORM,"Invalid option -%c",optopt);
-	    default: break;
-	}
-    } while (i!=-1);
+  /* process args */
+  opterr = 0;
+  optind = 0;
+  do {
+    i=getopt(argc,argv,opt_string);
+    switch (i) {
+    case 'q': if (j)
+      msg(MSG,"uses timer board timer");
+    else
+      msg(MSG,"uses a QNX timer");
+      DONE_MSG;
+      break;
+    case '?': msg(MSG_EXIT_ABNORM,"Invalid option -%c",optopt);
+    default: break;
+    }
+  } while (i!=-1);
 
-    if (optind >= argc)
-	msg(MSG_EXIT_ABNORM,"No solenoid file specified on command line");
+  if (optind >= argc)
+    msg(MSG_EXIT_ABNORM,"No solenoid file specified on command line");
 
-    for (exp_ext = 0; optind < argc; optind++) {
-      if (exp_ext != 0)  msg(MSG_EXIT_ABNORM,"Too many input files");
-      for (j = 0; argv[optind][j] != '\0'; j++) {
-        if (argv[optind][j] == '/') exp_ext = 0;
-        else if (argv[optind][j] == '.') exp_ext = j;
-        filename[j] = argv[optind][j];
+  for (exp_ext = 0; optind < argc; optind++) {
+    if (exp_ext != 0)  msg(MSG_EXIT_ABNORM,"Too many input files");
+    for (j = 0; argv[optind][j] != '\0'; j++) {
+      if (argv[optind][j] == '/') exp_ext = 0;
+      else if (argv[optind][j] == '.') exp_ext = j;
+      filename[j] = argv[optind][j];
+    }
+    if (exp_ext == 0) strcpy(filename+j, ".sft");
+    else filename[j] = '\0';
+    exp_ext = 1;
+    i = read_sft(filename);
+    if (i==1) msg(MSG_EXIT_ABNORM,"Can't open %s",basename(filename));
+    if (i==-1) msg(MSG_EXIT_ABNORM,"Wrong version: %s",basename(filename));
+  }
+
+  switch (toupper(which)) {
+  case 'A': reg_which = SOLDRV_PROXY_A; dct = DCT_SOLDRV_A; break;
+  case 'B': reg_which = SOLDRV_PROXY_B; dct = DCT_SOLDRV_B;  break;
+  case 'C': reg_which = SOLDRV_PROXY_C; dct = DCT_SOLDRV_C;  break;
+  case 'D': reg_which = SOLDRV_PROXY_D; dct = DCT_SOLDRV_D;  break;
+  case 'E': reg_which = SOLDRV_PROXY_E; dct = DCT_SOLDRV_E;  break;
+  case 'F': reg_which = SOLDRV_PROXY_F; dct = DCT_SOLDRV_F;  break;
+  case 'G': reg_which = SOLDRV_PROXY_G; dct = DCT_SOLDRV_G;  break;
+  case 'H': reg_which = SOLDRV_PROXY_H; dct = DCT_SOLDRV_H;  break;
+  case 'I': reg_which = SOLDRV_PROXY_I; dct = DCT_SOLDRV_I;  break;
+  case 'J': reg_which = SOLDRV_PROXY_J; dct = DCT_SOLDRV_J;  break;
+  default: msg(MSG_EXIT_ABNORM,"unknown SOLDRV_PROXY type %s", optarg);
+  }
+
+  /* attach timer */
+  ATTACH_TIMER;
+
+  /* look for subbus */
+  if (n_set_points) {
+    set_response(NLRSP_QUIET);	/* for norts Col_set_pointer */    		
+    for (j=0,i=0;i<n_set_points;i++)
+      if (set_points[i].address==0) {
+	j++;
+	if (!(Col_set_pointer(dct, &stat, 0)))
+	  msg(MSG,"achieved cooperation with DG");
+	else msg(MSG_WARN,"Can't cooperate with DG");
+	break;
       }
-      if (exp_ext == 0) strcpy(filename+j, ".sft");
-      else filename[j] = '\0';
-      exp_ext = 1;
-      i = read_sft(filename);
-      if (i==1) msg(MSG_EXIT_ABNORM,"Can't open %s",basename(filename));
-      if (i==-1) msg(MSG_EXIT_ABNORM,"Wrong version: %s",basename(filename));
+    if (n_set_points > j) {	
+      if (seteuid(0)==-1) msg(MSG_EXIT_ABNORM,"Can't set euid to root");
+      if (!load_subbus())
+	msg(MSG_EXIT_ABNORM,"Requires resident Subbus Library");
     }
+  }
 
-    switch (toupper(which)) {
-	case 'A': reg_which = SOLDRV_PROXY_A; dct = DCT_SOLDRV_A; break;
-	case 'B': reg_which = SOLDRV_PROXY_B; dct = DCT_SOLDRV_B;  break;
-	case 'C': reg_which = SOLDRV_PROXY_C; dct = DCT_SOLDRV_C;  break;
-	case 'D': reg_which = SOLDRV_PROXY_D; dct = DCT_SOLDRV_D;  break;
-	case 'E': reg_which = SOLDRV_PROXY_E; dct = DCT_SOLDRV_E;  break;
-	case 'F': reg_which = SOLDRV_PROXY_F; dct = DCT_SOLDRV_F;  break;
-	case 'G': reg_which = SOLDRV_PROXY_G; dct = DCT_SOLDRV_G;  break;
-	case 'H': reg_which = SOLDRV_PROXY_H; dct = DCT_SOLDRV_H;  break;
-	case 'I': reg_which = SOLDRV_PROXY_I; dct = DCT_SOLDRV_I;  break;
-	case 'J': reg_which = SOLDRV_PROXY_J; dct = DCT_SOLDRV_J;  break;
-	default: msg(MSG_EXIT_ABNORM,"unknown SOLDRV_PROXY type %s", optarg);
-    }
+  /* Look for SCDC */
+  if (n_solenoids)
+    if ( (scdc_tid = qnx_name_locate(getnid(),LOCAL_SYMNAME(SCDC),0,0)) == -1)
+      msg(MSG_EXIT_ABNORM,"Can't find symbolic name for %s",SCDC);
 
-    /* attach timer */
-    ATTACH_TIMER;
+  /* Look for cmdctrl */
+  cmd_tid=cc_init_options(argc,argv,dct,dct,reg_which,reg_which,QUIT_ON_QUIT);
 
-    /* look for subbus */
-    if (n_set_points) {
-	if (seteuid(0)==-1) msg(MSG_EXIT_ABNORM,"Can't set euid to root");
-	if (!load_subbus())
-	    msg(MSG_EXIT_ABNORM,"Requires resident Subbus Library");
-    }
+  /* sort messages by priority */
+  /*    qnx_pflags(~0,_PPF_PRIORITY_REC,0,0);*/
 
-    /* Look for SCDC */
-    if (n_solenoids)
-	if ( (scdc_tid = qnx_name_locate(getnid(),LOCAL_SYMNAME(SCDC),0,0)) == -1)
-	    msg(MSG_EXIT_ABNORM,"Can't find symbolic name for %s",SCDC);
-
-    /* Look for cmdctrl */
-    cmd_tid=cc_init_options(argc,argv,dct,dct,reg_which,reg_which,QUIT_ON_QUIT);
-
-    /* sort messages by priority */
-/*    qnx_pflags(~0,_PPF_PRIORITY_REC,0,0);*/
-
-    /* set up proxies */
-    if (n_proxies) 
-	if ( !(proxy_pids = (pid_t *) calloc(n_proxies, sizeof(pid_t))))
-	    msg(MSG_EXIT_ABNORM,"Can't allocate space for proxies");
+  /* set up proxies */
+  if (n_proxies) 
+    if ( !(proxy_pids = (pid_t *) calloc(n_proxies, sizeof(pid_t))))
+      msg(MSG_EXIT_ABNORM,"Can't allocate space for proxies");
 
 
-    /* program code */
+  /* program code */
+  for (;;) {
+    switch (state) {
+    case ST_NO_MODE:
+      /* wait for a msg from anyone except timer */
+      msg(MSG,"No Mode");
+      waitmsg(0);
+      break;
+    case ST_NEW_MODE:
+      crnt_mode = new_mode;
+      ip = mode_indices[new_mode];
+      state = ST_MODE;
+      mode_mode = ST_IN_MODE;
+      mode_request = 0;
+      msg(MSG,"Mode %d",crnt_mode);
+    case ST_MODE:
+    default:
+      /* In a Mode */
+      switch (mode_mode) {
+      case ST_IN_MODE:
+      default:
+	switch (mode_code[ip]) {
+	case SOL_STROBES:
+	  /* send discreet commands to SCDC */
+	  _setmx(&smsg[0],&hdr_dasc,sizeof(msg_hdr_type));
+	  _setmx(&smsg[1],&type_scdc,sizeof(unsigned char));
+	  ip++;
+	  _setmx(&smsg[2],&mode_code[ip],sizeof(unsigned char));
+	  while ( Sendmx( scdc_tid, 3, 1, smsg, &rmsg) ==-1)
+	    msg((errno==EINTR) ? MSG_WARN : MSG_EXIT_ABNORM,"Error sending to %s",SCDC);
+	  if (replycode != DAS_OK)
+	    msg(MSG_WARN,"Bad response from %s",SCDC);
+	  ip++;
+	  break;
+	case SOL_MULT_STROBES:
+	  _setmx(&smsg[0],&hdr_mult,sizeof(msg_hdr_type));
+	  i = mode_code[++ip];
+	  if (mode_code[ip] > (MAX_MSG_SIZE-2)) {
+	    msg(MSG_WARN,"multiple strobe command of length %d too long, shortened",mode_code[ip]+2);
+	    i = MAX_MSG_SIZE - 2;
+	  }
+	  _setmx(&smsg[1],&i,1);
+	  ip++;
+	  _setmx(&smsg[2],&mode_code[ip],i);
+	  while (Sendmx(scdc_tid,3,1,smsg,&rmsg)==-1)
+	    msg((errno==EINTR) ? MSG_WARN : MSG_EXIT_ABNORM,"Error sending to %s",SCDC);
+	  if (replycode != DAS_OK)
+	    msg(MSG_WARN,"Bad response from %s",SCDC);
+	  ip+=mode_code[ip-1];
+	  break;
+	case SOL_WAIT:
+	  msg(MSG_DEBUG,"Step %d wait", ip);
+	  waitmsg(timer_proxy);
+	  msg(MSG_DEBUG,"Step %d", ip);
+	  ip++;
+	  break;
+	case SOL_SET_TIME:
+	  /* resolution and set timer going */
+	  count = mode_code[ip+1] + (mode_code[ip+2] << 8);
+	  RESOLUTION_TIMER;
+	  /* up timer */
+	  UP_TIMER;
+	  ip += 3;
+	  break;
+	case SOL_GOTO:
+	  ip = mode_code[ip+1] + (mode_code[ip+2] << 8);
+	  break;
+	case SOL_END_MODE:
+	  state = ST_NO_MODE;
+	  DOWN_TIMER;
+	  msg(MSG,"Installed");
+	  break;
+	case SOL_WAITS:
+	  count = mode_code[ip+1];
+	  mode_mode = ST_WAITS;
+	  ip += 2;
+	  break;
+	case SOL_SELECT:
+	  DOWN_TIMER;
+	  if (mode_indices[mode_code[ip+1]]!=-1) {
+	    state = ST_NEW_MODE;
+	    new_mode = mode_code[ip+1];
+	  } else {
+	    if (mode_code[ip+1]!=0)
+	      msg(MSG_WARN,"can't select undefined mode %d",mode_code[ip+1]);
+	    state = ST_NO_MODE;
+	  }
+	  break;
+	case SOL_MSWOK:
+	  if (mode_request != 0) {
+	    DOWN_TIMER;
+	    state = ST_NEW_MODE;
+	  } else ip++;
+	  break;
+	case SOL_DTOA:
+	  if (set_points[mode_code[ip+1]].address == 0) {
+	    msg(MSG_DBG(1),"setting status to %d",set_points[mode_code[ip+1]].value);
+	    stat=set_points[mode_code[ip+1]].value;
+	  }
+	  else
+	    write_subbus(0,set_points[mode_code[ip+1]].address,
+			 set_points[mode_code[ip+1]].value);
+	  ip += 2;
+	  break;
+	case SOL_PROXY:
+	  if (proxy_pids[mode_code[++ip]])
+	    if (Trigger(proxy_pids[mode_code[ip]])==-1)
+	      msg(MSG_WARN,"Can't trigger proxy %d",proxy_pids[ip]);
+	  ip++;
+	  break;
+	}			/* switch */
+	break;
+      case ST_WAITS:
+	msg(MSG_DEBUG,"Step %d waits", ip);
+	waitmsg(timer_proxy);
+	msg(MSG_DEBUG,"Step %d", ip);
 	if (--count <= 0) mode_mode = ST_IN_MODE;
-	switch (state) {
-	    case ST_NO_MODE:
-		/* wait for a msg from anyone except timer */
-		msg(MSG,"No Mode");
-		waitmsg(0);
-		break;
-	    case ST_NEW_MODE:
-		crnt_mode = new_mode;
-		ip = mode_indices[new_mode];
-		state = ST_MODE;
-		mode_mode = ST_IN_MODE;
-		mode_request = 0;
-		msg(MSG,"Mode %d",crnt_mode);
-	    case ST_MODE:
-	    default:
-	    /* In a Mode */
-	    switch (mode_mode) {
-		case ST_IN_MODE:
-		default:
-		switch (mode_code[ip]) {
-		    case SOL_STROBES:
-			/* send discreet commands to SCDC */
-			_setmx(&smsg[0],&hdr_dasc,sizeof(msg_hdr_type));
-			_setmx(&smsg[1],&type_scdc,sizeof(unsigned char));
-			ip++;
-			_setmx(&smsg[2],&mode_code[ip],sizeof(unsigned char));
-			while ( Sendmx( scdc_tid, 3, 1, smsg, &rmsg) ==-1)
-			    msg((errno==EINTR) ? MSG_WARN : MSG_EXIT_ABNORM,"Error sending to %s",SCDC);
-			if (replycode != DAS_OK)
-			    msg(MSG_WARN,"Bad response from %s",SCDC);
-			ip++;
-			break;
-		    case SOL_MULT_STROBES:
-			_setmx(&smsg[0],&hdr_mult,sizeof(msg_hdr_type));
-			i = mode_code[++ip];
-			if (mode_code[ip] > (MAX_MSG_SIZE-2)) {
-			    msg(MSG_WARN,"multiple strobe command of length %d too long, shortened",mode_code[ip]+2);
-			    i = MAX_MSG_SIZE - 2;
-			}
-			_setmx(&smsg[1],&i,1);
-			ip++;
-			_setmx(&smsg[2],&mode_code[ip],i);
-			while (Sendmx(scdc_tid,3,1,smsg,&rmsg)==-1)
-			    msg((errno==EINTR) ? MSG_WARN : MSG_EXIT_ABNORM,"Error sending to %s",SCDC);
-			if (replycode != DAS_OK)
-			    msg(MSG_WARN,"Bad response from %s",SCDC);
-			ip+=mode_code[ip-1];
-			break;
-		    case SOL_WAIT:
-			msg(MSG_DEBUG,"Step %d wait", ip);
-			waitmsg(timer_proxy);
-			msg(MSG_DEBUG,"Step %d", ip);
-			ip++;
-			break;
-		    case SOL_SET_TIME:
-			/* resolution and set timer going */
-			count = mode_code[ip+1] + (mode_code[ip+2] << 8);
-			RESOLUTION_TIMER;
-			/* up timer */
-			UP_TIMER;
-			ip += 3;
-			break;
-		    case SOL_GOTO:
-			ip = mode_code[ip+1] + (mode_code[ip+2] << 8);
-			break;
-		    case SOL_END_MODE:
-			state = ST_NO_MODE;
-			DOWN_TIMER;
-			msg(MSG,"Installed");
-			break;
-		    case SOL_WAITS:
-			count = mode_code[ip+1];
-			mode_mode = ST_WAITS;
-			ip += 2;
-			break;
-		    case SOL_SELECT:
-			DOWN_TIMER;
-			if (mode_indices[mode_code[ip+1]]!=-1) {
-			    state = ST_NEW_MODE;
-			    new_mode = mode_code[ip+1];
-			} else {
-			    if (mode_code[ip+1]!=0)
-				msg(MSG_WARN,"can't select undefined mode %d",mode_code[ip+1]);
-			    state = ST_NO_MODE;
-			}
-			break;
-		    case SOL_MSWOK:
-			if (mode_request != 0) {
-			    DOWN_TIMER;
-			    state = ST_NEW_MODE;
-			    } else ip++;
-			break;
-		    case SOL_DTOA:
-			write_subbus(0,set_points[mode_code[ip+1]].address,
-			set_points[mode_code[ip+1]].value);
-			ip += 2;
-			break;
-		    case SOL_PROXY:
-			if (proxy_pids[mode_code[++ip]])
-			    if (Trigger(proxy_pids[mode_code[ip]])==-1)
-				msg(MSG_WARN,"Can't trigger proxy %d",proxy_pids[ip]);
-			ip++;
-			break;
-		    } /* switch */
-		    break;
-		case ST_WAITS:
-		    msg(MSG_DEBUG,"Step %d waits", ip);
-		    waitmsg(timer_proxy);
-		    msg(MSG_DEBUG,"Step %d", ip);
-		    if (--count <= 0) mode_mode = ST_IN_MODE;
-            
-		} /* switch */
-	    } /* switch */
-	} /* for(;;) */
+      }				/* switch */
+    }				/* switch */
+  }				/* for(;;) */
 
-} /* main */
+}				/* main */
 
 /*
 Wait for msg from 'invite'. Typically from cmdctrl, timer or processes
