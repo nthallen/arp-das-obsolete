8a6eae2617ecfff93afa910162ba213774ffbc22
Norton Allen <allen@huarp.harvard.edu>
1995-10-06 16:22:31 +0000

    Modified nlcon_display() to support row, col on variable size consoles.

--- QNX4/includes/nl_cons.h
+++ QNX4/includes/nl_cons.h
@@ -1,6 +1,9 @@
 /* nl_cons.h defines functions required for general use of
  * QNX consoles.
  * $Log$
+ * Revision 1.3  1995/09/07  20:10:47  nort
+ * Added rows, columns to nl_con_def struct
+ *
  * Revision 1.2  1994/11/22  14:55:07  nort
  * Minor argument change.
  *
@@ -33,7 +36,8 @@ extern nl_con_def nl_cons[MAXCONS];
 extern int nlcons_defined;
 extern char * opt_string;
 void Con_init_options(int argcc, char **argvv); /* coninit.c */
-void nlcon_display(unsigned int index, int offset, const char *s, char attr); /* coninit.c */
+void nlcon_display(unsigned int index, int row, int col,
+				   const char *s, char attr); /* coninit.c */
 int nlcon_ctrl(unsigned int index, struct _console_ctrl **con_ctrl); /* coninit.c */
 int nlcon_getch(void); /* congetch.c */
 void nlcg_receive(pid_t who); /* cgrecv.c */
