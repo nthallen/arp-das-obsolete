0d0e8df1774e0eb191b071e2c6e90de52558452c
Norton Allen <allen@huarp.harvard.edu>
1995-10-31 19:04:01 +0000

    Support for CmdCtrl proxy_on_quit

--- QNX4/nortlib/kbdclt.c
+++ QNX4/nortlib/kbdclt.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <sys/kernel.h>
+#include <sys/proxy.h>
 #include <stdlib.h>
 #include "nortlib.h"
 #include "cmdctrl.h"
@@ -13,10 +14,10 @@
 int kbdclt_quit( int argc, char **argv ) {
   int c;
   int resp;
-  nid_t cis_node;
+  nid_t my_node, cis_node;
   pid_t cc_pid;
 
-  cis_node = getnid();
+  my_node = cis_node = getnid();
   optind = 0;
   opterr = 0;
   if (argc > 0) do {
@@ -37,12 +38,19 @@ int kbdclt_quit( int argc, char **argv ) {
 
     ccr.ccreg_byt = CCReg_MSG;
     ccr.min_dasc = ccr.max_dasc = ccr.min_msg = ccr.max_msg = 0;
-    ccr.how_to_quit = FORWARD_QUIT;
+    ccr.how_to_quit = PROXY_ON_QUIT;
     ccr.how_to_die = NOTHING_ON_DEATH;
-	if ( Send( cc_pid, &ccr, &rv, sizeof( ccr ), sizeof( rv ) ) != 0 )
-	  nl_error( 1, "Error Sending to CmdCtrl" );
-	else if ( rv != DAS_OK )
-	  nl_error( 1, "Error return from CmdCtrl: %d", rv );
+	ccr.proxy = qnx_proxy_attach( 0, NULL, 0, -1 );
+	if ( ccr.proxy != -1 && cis_node != my_node)
+	  ccr.proxy = qnx_proxy_rem_attach( cis_node, ccr.proxy );
+	if ( ccr.proxy == -1 )
+	  nl_error( 2, "Unable to create proxy for cmdctrl" );
+	else {
+	  if ( Send( cc_pid, &ccr, &rv, sizeof( ccr ), sizeof( rv ) ) != 0 )
+		nl_error( 1, "Error Sending to CmdCtrl" );
+	  else if ( rv != DAS_OK )
+		nl_error( 1, "Error return from CmdCtrl: %d", rv );
+	}
   } else nl_error( -2, "Unable to locate " CMD_CTRL );
   set_response( resp );
   return cc_pid;
