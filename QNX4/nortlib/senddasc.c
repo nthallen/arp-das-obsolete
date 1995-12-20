a9a3f6ea5d4ec6ab3d3f8e34972926a4bc33e64d
Norton Allen <allen@huarp.harvard.edu>
1995-12-20 03:02:26 +0000

    Needed sys/types.h for changes to cmdctrl.h

--- QNX4/nortlib/senddasc.c
+++ QNX4/nortlib/senddasc.c
@@ -3,18 +3,20 @@
  * is handled according to nl_response rules. Valid error returns
  * from CC (or DG) are returned to the calling program.
  * $Log$
+ * Revision 1.2  1993/07/01  15:35:04  nort
+ * Eliminated "unreferenced" via Watcom pragma
+ *
  * Revision 1.1  1992/09/02  13:26:38  nort
  * Initial revision
  *
  */
+#include <sys/types.h>
 #include "cmdctrl.h"
 #include "nortlib.h"
-#ifdef __WATCOMC__
-  #pragma off (unreferenced)
-	static char rcsid[] =
-	  "$Id$";
-  #pragma on (unreferenced)
-#endif
+#pragma off (unreferenced)
+  static char rcsid[] =
+	"$Id$";
+#pragma on (unreferenced)
 
 int send_dascmd(int type, int value, int dg_ok) {
   dasc_msg_type dm;
