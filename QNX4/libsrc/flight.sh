e8fd5e5f27e8eb91be8c19dda9287c9c4abd808d
Norton Allen <allen@huarp.harvard.edu>
1996-01-05 14:41:20 +0000

    Launch function, other minor mods.

--- QNX4/libsrc/flight.sh
+++ QNX4/libsrc/flight.sh
@@ -3,10 +3,11 @@
 #__USAGE
 #%C
 export PATH=:/bin32:/bin:/usr/bin:/usr/local/bin
+echo "\nRunning flight.sh: \c"; date
 #----------------------------------------------------------------
 # Make the console readable so we can ditto it easily if necessary
 #----------------------------------------------------------------
-chmod a+r `tty`
+chmod a+r `tty` 2> /dev/null
 #----------------------------------------------------------------
 # This is where we will decide what experiment we are
 #----------------------------------------------------------------
@@ -25,8 +26,32 @@ else
 fi
 export Experiment
 
+typeset launch_error=""
+
+function Launch {
+  name=$1
+  shift
+  [ -n "$launch_error" ] && return 1
+  [ -n "$VERBOSE" ] && echo "Launch: $*"
+  if { $* & }; then
+	if [ "$name" != "-" ]; then
+	  [ -n "$VERBOSE" ] && echo "Launch: Waiting for $!:$name"
+	  namewait -p $! -t 20 $name || {
+		echo "Launch namewait failure: $*" >&2
+		launch_error=yes
+		return 1
+	  }
+	fi
+  else
+	echo "Launch Error: $*" >&2
+	launch_error=yes
+	return 1
+  fi
+  return 0
+}
+
 umask g+w
-[ -n "FlightNode" ] && namewait -n0 pick_file
+[ -n "$FlightNode" ] && namewait -n0 pick_file
 script=`pick_file -C`
 if [ -r "$script" ]; then
   echo flight.sh: `id`: Experiment=$Experiment script=$script
@@ -34,7 +59,7 @@ if [ -r "$script" ]; then
 else
   echo flight.sh: Specified script $script not found >&2
 fi
-pick_file -q
+[ -z "$launch_error" ] && pick_file -q
 
 typeset qoc
 [ -n "$FlightNode" ] && qoc="-q"
