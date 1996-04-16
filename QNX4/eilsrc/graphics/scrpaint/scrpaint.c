faea615b9966a3e198f8a8bd98ed03959009a84a
Eileen Schomp <eil@huarp.harvard.edu>
1996-04-16 19:45:40 +0000

    check more error returns

--- QNX4/eilsrc/graphics/scrpaint/scrpaint.c
+++ QNX4/eilsrc/graphics/scrpaint/scrpaint.c
@@ -13,52 +13,64 @@ unsigned char attributes[MAX_ATTRS];
 #define HDR "paint"
 
 char *extfilename(char *name, char *ext, char *ans) {
-char *p;
-strcpy(ans,name);
-/* if there is an extension already */
-if ((p=strchr(ans,(int)'.'))!=0)
-	*p='\0';
-strcat(ans,".");
-strcat(ans,ext);
-return(ans);
+  char *p;
+  strcpy(ans,name);
+  /* if there is an extension already */
+  if ((p=strchr(ans,(int)'.'))!=0)
+    *p='\0';
+  strcat(ans,".");
+  strcat(ans,ext);
+  return(ans);
 }
 
 main(int argc, char **argv) {
-WINDOW *stdcodescr;
-struct hdr shdr;
-int i;
-char space2[NAME_MAX];
-extern int optind;
+  WINDOW *stdcodescr;
+  struct hdr shdr;
+  int i;
+  char space2[NAME_MAX];
+  extern int optind;
 
-msg_init_options(HDR,argc,argv);
-msg(MSG,"task %d: started, and will terminate shortly",getpid());
-shdr.nattrs=0;
-initscr();
-scrollok(stdscr,FALSE);
-stdcodescr=newwin(LINES,COLS,0,0);
-/* first check for .scr or .fld extensions */
-if (strstr(argv[optind],".scr"))
-    scr_win_in(argv[optind], stdcodescr, 0, -1, -1, &shdr);
-else if (strstr(argv[optind],".fld"))
-    fld_win_in(argv[optind], stdcodescr, 0, 0, 0, -1, -1, &shdr);
-else
+  msg_init_options(HDR,argc,argv);
+  msg(MSG,"task %d: started, and will terminate shortly",getpid());
+  shdr.nattrs=0;
+  if (initscr()==ERR) msg(MSG_FATAL,"Cannot curses initialise");
+  scrollok(stdscr,FALSE);
+  if ( (stdcodescr=newwin(LINES,COLS,0,0)) == NULL)
+    msg(MSG_FATAL,"Cannot create new curses window");
+
+  /* first check for .scr or .fld extensions */
+  if (strstr(argv[optind],".scr")) {
+   if (!scr_win_in(argv[optind], stdcodescr, 0, -1, -1, &shdr))
+     msg(MSG_FATAL,"Error creating screen from scr file");
+  }
+  else if (strstr(argv[optind],".fld")) {
+    if (!fld_win_in(argv[optind], stdcodescr, 0, 0, 0, -1, -1, &shdr))
+      msg(MSG_FATAL,"Error creating screen from fld file");
+  }
+  else
     if ( !scr_win_in(argv[optind], stdcodescr, 0, -1, -1, &shdr) )
-	if ( !fld_win_in(argv[optind], stdcodescr, 0, 0, 0, -1, -1, &shdr) )
-	    if ( !scr_win_in(extfilename(argv[optind],"scr",space2), stdcodescr, 0, -1, -1, &shdr) )
-		fld_win_in(extfilename(argv[optind],"fld",space2), stdcodescr, 0, 0, 0, -1, -1, &shdr);
-if (shdr.nattrs) {
+      if ( !fld_win_in(argv[optind], stdcodescr, 0, 0, 0, -1, -1, &shdr) )
+	if ( !scr_win_in(extfilename(argv[optind],"scr",space2),
+			 stdcodescr, 0, -1, -1, &shdr) )
+	  if (!fld_win_in(extfilename(argv[optind],"fld",space2),
+		     stdcodescr, 0, 0, 0, -1, -1, &shdr))
+	    msg(MSG_FATAL,"Error creating screen from fld file");
+
+  if (shdr.nattrs) {
     if (argc > optind+1) {
-	if (!init_attrs(argv[optind+1],attributes,shdr.nattrs))
-	    if (!init_attrs(extfilename(argv[optind+1],"cfg",space2),attributes,shdr.nattrs))
-		msg(MSG_FAIL,"error reading cfg of %s",argv[optind+1]);
+      if (!init_attrs(argv[optind+1],attributes,shdr.nattrs))
+	if (!init_attrs(extfilename(argv[optind+1],"cfg",space2),
+			attributes,shdr.nattrs))
+	  msg(MSG_FAIL,"error reading cfg of %s",argv[optind+1]);
     }
-    else init_attrs(extfilename(argv[optind],"cfg",space2),attributes,shdr.nattrs);
+    else init_attrs(extfilename(argv[optind],"cfg",space2),
+		    attributes,shdr.nattrs);
     overwrite(stdcodescr,stdscr);
     WrtAttrTable(stdscr,stdcodescr,attributes,-1,-1,shdr.rows,shdr.cols);
     refresh();
-} else msg(MSG_FAIL,"error reading scr or fld of %s",argv[optind]);
-delwin(stdcodescr);
-term_restore();
-term_cur(LINES-1,0);
-endwin();
+  } else msg(MSG_FAIL,"error reading scr or fld of %s",argv[optind]);
+  delwin(stdcodescr);
+  term_restore();
+  term_cur(LINES-1,0);
+  endwin();
 }
