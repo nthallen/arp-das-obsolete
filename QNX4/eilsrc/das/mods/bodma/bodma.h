c1b92f82c1f30b87e900b357372285cf54496409
Eileen Schomp <eil@huarp.harvard.edu>
1997-05-29 15:13:26 +0000

    Header File for Priority Message Version of Bodma Driver

--- QNX4/eilsrc/das/mods/bodma/bodma.h
+++ QNX4/eilsrc/das/mods/bodma/bodma.h
@@ -2,9 +2,21 @@
 
 #include "port_types.h"
 
-#define BODMA_STRING "bodma_data"  /* used with nort's Col_send_init */
+#define BODMA_STRING "Bo"  /* used with nort's Col_send_init */
 #define BODMA "bodma"                   /* my attached name */
 #define SEQ36 25
+
+
+/* An Acqusition Request specifies number of Coadd Sequences and Number
+   of Scans to Coadd for each Sequence */
+/* Status */
+#define BO_CLOSE 0 /* Driver Not Installed */
+#define BO_READY 1 /* Driver Ready for Acquisition Requests */
+#define BO_INIT 2  /* Driver Initialising */
+#define BO_START 3 /* Driver Starting an Acquisition Request */
+#define BO_ACQ 4   /* Driver Acquiring Request, Awaiting Data */
+#define BO_DATA 5  /* A Coadd Sequence is Available */
+#define BO_LOG 6   /* Driver Logging Data */
 
 /* Structure that is passed to Collection */
 typedef struct {
@@ -15,7 +27,8 @@ typedef struct {
 
 typedef struct {
   BYTE1 hdr;
-  BYTE4 scans;/* 0 : single IR; >0 : double IR, # scans/coadd; <0 : stop */
+  BYTE2 scans;/* 0 : single IR; >0 : double IR, # scans/coadd; <0 : stop */
+  UBYTE2 runs; /* Number of Measurements */
   BYTE1 david_code;
   BYTE1 david_pad[8];
 } Seq36_Req;
