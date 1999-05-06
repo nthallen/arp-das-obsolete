a863d3df0f3d8958ba95ef2b2edda0600c39db5f
Norton Allen <allen@huarp.harvard.edu>
1999-05-06 19:27:42 +0000

    Added chksig.html, moved init to SIGNAL::siginit()

--- Nets/nets2html.bat
+++ Nets/nets2html.bat
@@ -29,86 +29,13 @@ use HTML;
 use File::Copy;
 my $warn_buficon = 1;
 my @html_refs = ( "index:Components", "master:Master",
-				  "SIG_A-C:Signals" );
+				  "SIG_A-C:Signals", "chksig:Check" );
 my @mon = ( "January", "February", "March", "April", "May",
          "June", "July", "August", "September", "October",
 		 "November", "December" );
 
-# access registry to locate Nets project dir
-# This code is used by drawsch.bat also. Should really go in a
-# library routine.
-my $nets_dir = '';
-{ use Win32::Registry;
-
-  my @keys;
-  sub getsubkey {
-    my ( $key, $subkey ) = @_;
-    my $newkey;
-    ${$key}->Open( $subkey, $newkey ) || return 0;
-    push( @keys, $newkey );
-    $$key = $newkey;
-    return 1;
-  }
-  my $key = $main::HKEY_CURRENT_USER;
-  foreach my $subkey ( qw( Software HUARP Nets BaseDir ) ) {
-    die "Nets Project Directory is undefined (in Registry)\n"
-      unless getsubkey( \$key, $subkey );
-  }
-  my %vals;
-  $key->GetValues( \%vals ) || die "GetValues failed\n";
-  $nets_dir = $vals{''}->[2];
-  
-  while ( $key = pop(@keys) ) {
-    $key->Close;
-  }
-}
-die "Unable to locate nets project directory\n"
-  unless $nets_dir && -d $nets_dir && chdir $nets_dir;
-
-my $logfile = "nets2html";
-
-open( LOGFILE, ">$logfile.err" ) ||
-  die "Unable to open log file\n";
-
-$SIG{__WARN__} = sub {
-  print LOGFILE @_;
-  warn @_;
-};
-
-sub LogMsg {
-  print LOGFILE @_;
-  print STDERR @_;
-}
-
-$SIG{__DIE__} = sub {
-  warn @_;
-  print STDERR "\nHit Enter to continue:";
-  my $wait = <STDIN>;
-  print STDERR "\n";
-  exit(1);
-};
-
-END {
-  if ( defined $SIG{__WARN__} ) {
-	delete $SIG{__WARN__};
-	delete $SIG{__DIE__};
-	close LOGFILE;
-	unlink( "$logfile.bak" );
-	rename( "$logfile.err", "$logfile.bak" );
-	open( IFILE, "<$logfile.bak" ) ||
-	  die "Unable to read $logfile.bak";
-	open( OFILE, ">$logfile.err" ) ||
-	  die "Unable to rewrite $logfile.err";
-	print OFILE
-	  map $_->[0],
-		sort { $a->[1] cmp $b->[1] || $a->[0] cmp $b->[0] }
-		  map { $_ =~ m/:\s+(.*)$/ ? [ $_, $1 ] : [ $_, '' ] } <IFILE>;
-	close OFILE;
-	close IFILE;
-  }
-}
-
-LogMsg "Nets2HTML: $nets_dir\n";
+my $nets_dir = SIGNAL::siginit( "nets2html", 1, shift @ARGV );
+SIGNAL::LogMsg "Nets2HTML: $nets_dir\n";
 
 $SIGNAL::context = 'Nets2html';
 SIGNAL::load_signals();
@@ -253,7 +180,7 @@ sub html_header {
   print
 	"<CENTER>\n",
 	$t->Output( 'html-tables' ),
-	html_trailer( '', 'Master' ),
+	html_trailer( '', 'master' ),
 	"</CENTER>\n",
 	end_html, "\n";
   close STDOUT || warn "$SIGNAL::context: Error closing html/master.html\n";
@@ -725,11 +652,17 @@ if ( open( STDOUT, ">html/chksig.html" ) ) {
   print
 	html_header( "$SIGNAL::global{Exp} Signal Cross-Check" ),
 	html_opt_header( "chksig.html",
-	  "<CENTER>Signal Cross-Check</CENTER>\n" ),
-	"\n<UL>\n";
+	  "<CENTER><H2>Signal Cross-Check</H2></CENTER>\n" ),
+	"<P>This page lists signal names which are associated\n",
+	"with more than one global signal or are associated with\n",
+	"one global signal of a different name. There may not be\n",
+	"anything wrong with either of these situations, but you\n",
+	"may want to double check.\n",
+	"\n";
 
   my @sigs = sort keys %SIGNAL::sigcomps;
   foreach my $signal ( @sigs ) {
+	my $desc = SIGNAL::get_sigdesc($signal) || '';
 	my %gn;
 	foreach my $comp ( keys %{$SIGNAL::sigcomps{$signal}} ) {
 	  my $locsig = "$signal($comp)";
@@ -737,32 +670,24 @@ if ( open( STDOUT, ">html/chksig.html" ) ) {
 	  $gn{$gn} = $locsig;
 	}
 	my @gn = sort keys %gn;
-	if ( @gn > 1 ) {
+	if ( $signal !~ m/^(_AD|\$)/ &&
+		 ( @gn > 1 ||
+		   $gn[0] !~ m/^\Q$signal\E(\(.*\))?$/ ) ) {
 	  print
-		"<LI>$signal\n<UL>\n";
+		"<P><B>$signal:</B> $desc\n<UL>\n";
 	  foreach my $gn ( @gn ) {
 		my $slice = SIGNAL::pick_slice($gn);
 		my $glink =
 		  "<A HREF=\"SIG_$slice.html#$gn\">$gn</A>";
 		print
-		  "<LI>$gn{$gn} => $glink\n";
+		  "<LI>$glink: $gn{$gn}\n";
 	  }
 	  print "</UL>\n";
-	} else {
-	  my $gn = $gn[0];
-	  $gn =~ s/\(.*\)$//;
-	  if ( $signal !~ /^\$/ && $gn ne $signal ) {
-		$gn = $gn[0];
-		my $slice = SIGNAL::pick_slice($gn);
-		my $glink =
-		  "<A HREF=\"SIG_$slice.html#$gn\">$gn</A>";
-		print "<LI>$signal => $glink\n";
-	  }
 	}
   }
   print
-	"</UL>\n",
-	html_trailer( '', 'ChkSig' ),
+	#"</UL>\n",
+	html_trailer( '', 'chksig' ),
 	end_html, "\n";
 } else {
   warn "$SIGNAL::context: Unable to create html/chksig.html\n";
