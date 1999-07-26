cf2bd08d6a9c845aaa1c4c1ee79fc0b9978a8921
Norton Allen <allen@huarp.harvard.edu>
1999-07-26 15:04:49 +0000

    Force use of qmake instead of gmake

--- QNX4/libsrc/libmaint.mk
+++ QNX4/libsrc/libmaint.mk
@@ -1,12 +1,15 @@
 # libmaint.mk is specific to making libraries
 # Should be referenced from /usr/local/bin/Makelib:
-#   make -f /usr/local/include/libmaint.mk $*
+#   qmake -f /usr/local/include/libmaint.mk $* MAKE=qmake
 # Usage:
 #   Makelib [every]
 #     Makes every model specified in MODELS
 #   Makelib clean|tidy
 #     Invokes maint.mk2 for each model.
 # $Log$
+# Revision 1.4  1994/04/18  17:57:25  nort
+# Removed last obsolete reference to WCC32
+#
 # Revision 1.3  1994/04/18  17:47:46  nort
 # Removed obsolete references to WCC32.
 # Elminated directory creation, since that is handled in library.mk
@@ -26,8 +29,9 @@ every :
 	fi; :
 	@for i in $(MODELS); do\
 	  echo Making MODEL $${i};\
-	  if [ $$i = 3r ]; then $(MAKE) MODEL=$$i MODELARGS="-ms -3";\
-	  else $(MAKE) MODEL=$$i;\
+	  if [ $$i = 3r ]; then \
+	  $(MAKE) MODEL=$$i MODELARGS="-ms -3" MAKE=qmake;\
+	  else $(MAKE) MODEL=$$i MAKE=qmake;\
 	  fi;\
 	done; :
 clean tidy :
