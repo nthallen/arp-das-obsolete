f972bbd8a4f60cef44c1348c1e2c5d96474d031d
Norton Allen <allen@huarp.harvard.edu>
1999-11-17 16:26:03 +0000

    Added getcon release and -g arg.

--- QNX4/nortlib/coninit.c
+++ QNX4/nortlib/coninit.c
@@ -4,10 +4,12 @@
 #include <sys/dev.h>
 #include <sys/proxy.h>
 #include "nl_cons.h"
+#include "nortlib.h"
 char rcsid_coninit_c[] =
 	"$Header$";
 
 static int n_cons = 1;
+static int release_cons = 1;
 nl_con_def nl_cons[MAXCONS];
 int nlcons_defined = 0;
 
@@ -29,10 +31,12 @@ static void nl_con_file(int index, char *name) {
 
   if (*name == '-') nl_cons[index].fd = -1;
   else {
-	fd = open(optarg, O_RDWR);
+	fd = open(name, O_RDWR);
 	if (fd < 0)
-	  nl_error(3, "Error opening console file %s", optarg);
+	  nl_error(3, "Error opening console file %s", name);
 	else nl_con_fd(index, fd);
+	if ( release_cons )
+	  getcon_release( name );
   }
 }
 
@@ -51,6 +55,9 @@ void Con_init_options(int argcc, char **argvv) {
 		if (n_cons == MAXCONS)
 		  nl_error(4, "Too many consoles specified on command line");
 		else nl_con_file(n_cons++, optarg);
+		break;
+	  case 'g':
+		release_cons = 0;
 		break;
 	  default:
 		break;
@@ -105,6 +112,13 @@ void Con_init_options(int argcc, char **argvv);
   all subsequent consoles. If any device argument is '-', that
   console is not opened and subsequent output to that index will
   be supressed.<P>
+  
+  After each console is opened, an attempt is made
+  to contact getcon to release the console. The 'g' option
+  supresses this release request for consoles appearing later in
+  the command line. This might be appropriate for short-lived
+  programs such as scrpaint (if it used nl_cons) where it is
+  desirable for getcon to keep the console open.<P>
   
   At present, these functions support only QNX consoles. At some
   point in the future, they may be switched over to use terminfo
