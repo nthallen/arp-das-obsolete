30162a5da93b4ffb21d7402427da513e8213abe7
Norton Allen <allen@huarp.harvard.edu>
2000-04-28 16:48:50 +0000

    Added documentation .,

--- QNX4/nortlib/kbdclt.c
+++ QNX4/nortlib/kbdclt.c
@@ -35,3 +35,36 @@ int kbdclt_quit( int argc, char **argv ) {
   } while ( c != -1 );
   return cc_quit_request( cis_node );
 }
+
+/*
+=Name kbdclt_quit(): Setup Keyboard Client to quit gracefully
+=Subject Client/Server
+=Subject Startup
+
+=Synopsis
+
+#include "cltsrvr.h"
+int kbdclt_quit( int argc, char **argv );
+
+=Description
+
+kbdclt_quit() is an init_options-kind-of-function which sets a
+keyboard client up to quit gracefully when an experiment
+terminates. It is referenced by kbdclt.oui. This function invokes
+a lower-level setup via the function =cc_quit_request=(). That
+function allocates an empty proxy and passes it to CmdCtrl or
+PBreg. The proxy will be triggered when the appropriate server
+receives a quit command.
+
+=Returns
+
+On success, the PID of the quit proxy is returned. On failure, if
+=nl_response= is set low enough to prevent an abort, -1 is
+returned.
+
+=SeeAlso
+
+=cc_quit_request=().
+
+=End
+*/
