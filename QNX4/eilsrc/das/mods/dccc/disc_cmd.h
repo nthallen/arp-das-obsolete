e735a1a4d36848836249bfd0376077bd3afd6b7d
Eileen Schomp <eil@huarp.harvard.edu>
2001-02-21 20:21:59 +0000

    MULTCMDS format

--- QNX4/eilsrc/das/mods/dccc/disc_cmd.h
+++ QNX4/eilsrc/das/mods/dccc/disc_cmd.h
@@ -8,17 +8,22 @@
 /*
    The following are the command types:
    SPARE indicates that dccc does not take any action
-   STRB  indicates the designated bit should be set and the strobe set also.
+   STRB  indicates the designated bits (mask) for a port should be set and
+         the strobe set also.
          These commands must be sent to dccc twice: once to start and once
          to stop.  The strobed command controller will do the necessary
          waiting.
-   STEP  indicates the designated bit is a motor step command.  The line
-         should be set and then immediately reset. (It is not anticipated that
-         any step commands will be issued directly from command reception, since
-         all of the steppable devices have drivers which keep track of step
-         number.  However, I will not remove the capability of stepping the
-         drives as a redundant feature.)
-   SET   indicates that the bit will be set or cleared.
+   STEP  indicates the designated bits (mask) is a motor step command.  The
+         lines should be set and then immediately reset. (It is not
+         anticipated that any step commands will be issued directly from
+         command reception, since all of the steppable devices have drivers
+         which keep track of step number.  However, I will not remove the
+         capability of stepping the drives as a redundant feature.)
+   SET   indicates that bits of a port will be set or cleared. If mask=0, the
+         bits are designated by given value. If mask != 0, then bits (mask) are
+         cleared if value == 0, else they are set.
+   SELECT indicates that select bits (mask) of a port are set to given value.
+         Selected bits are re-inverted.
    NDCC  indicates that this command does not a discrete command card command,
          but is rather some other type of one argument command.
 */
@@ -27,4 +32,5 @@
 #define STRB 1
 #define STEP 2
 #define SET 3
+#define SELECT 4
 /* #define NDCC 4 */
