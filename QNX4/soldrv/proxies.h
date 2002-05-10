328c6b42f07e10a2af8805588e6e44683840a435
Norton Allen <allen@huarp.harvard.edu>
2002-05-10 16:10:42 +0000

    Increased some arbitrary limits

--- QNX4/soldrv/proxies.h
+++ QNX4/soldrv/proxies.h
@@ -1,12 +1,15 @@
 /* proxies.h defines structures used for the Proxy statement.
  * $Log$
+ * Revision 1.2  1993/09/28  17:07:56  nort
+ * *** empty log message ***
+ *
  * Revision 1.1  1992/09/21  18:21:44  nort
  * Initial revision
  *
  */
-#define MAX_PROXY_POINTS 6
-#define MAX_PROXIES 10
-#define MAX_PRXY_PTS 15
+#define MAX_PROXY_POINTS 40
+#define MAX_PROXIES 20
+#define MAX_PRXY_PTS 50
 
 typedef struct {
   char *name;
