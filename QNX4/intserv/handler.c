6450d35f099bbbeae509e62eda3890469f736f3e
Norton Allen <allen@huarp.harvard.edu>
2005-10-24 16:57:55 +0000

    spare and pfail handlers

--- QNX4/intserv/handler.c
+++ QNX4/intserv/handler.c
@@ -4,3 +4,11 @@
 pid_t far expint_handler( void ) {
   return expint_proxy;
 }
+
+pid_t far spare_handler( void ) {
+  return spare_proxy;
+}
+
+pid_t far pfail_handler( void ) {
+  return pfail_proxy;
+}
