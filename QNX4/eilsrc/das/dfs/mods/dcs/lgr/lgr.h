856dfe7fbca3ff75ad0ed3f4f84b514962b3862f
Norton Allen <allen@huarp.harvard.edu>
2005-12-06 21:41:03 +0000

    Mods for comipile outside Eileen's hierarchy Removed old usage and used oui

---
+++ QNX4/eilsrc/das/dfs/mods/dcs/lgr/lgr.h
@@ -0,0 +1,7 @@
+/* lgr.h internal definitions */
+#ifndef LGR_H_INCLUDED
+#define LGR_H_INCLUDED
+
+void lgr_init_options( int argc, char **argv );
+
+#endif
--- QNX4/lgr/lgr.c
+++ QNX4/lgr/lgr.c
@@ -1,6 +1,7 @@
 /*
     lgr main module.
     Written by Eil for QNX 4 4/28/92.
+	Modified for use with oui by Nort 12/6/05
 */
 
 #include <stdlib.h>
@@ -20,13 +21,10 @@
 #include "das.h"
 #include "dbr.h"
 #include "dbr_mod.h"
-
-/* defines */
-#define HDR "lgr"
-#define OPT_MINE "wd:r:N:z:L:F:"
+#include "lgr.h"
+#include "oui.h"
 
 /* global variables */
-char *opt_string=OPT_DC_INIT OPT_MSG_INIT OPT_CC_INIT OPT_MINE;
 int logging;		/* logging enable switch */
 long maxfilesize;	/* maximum allowable log file size */
 int fcount;		/* file count */
@@ -35,76 +33,75 @@ char dirname[FILENAME_MAX];
 char name[FILENAME_MAX];
 int filesperdir;
 
+void lgr_init_options( int argc, char **argv ) {
+  /* getopt variables */
+  extern char *optarg;
+  extern int optind, opterr, optopt;
+
+  /* local variables */	
+  int  i, wflag;
+
+  /* initialisations */
+  strcpy(rootname,ROOTNAME);
+  getcwd(dirname,FILENAME_MAX);
+  wflag=0;
+  logging = 1;
+  fcount = 0;
+  filesperdir = FILESPERDIR;
+  maxfilesize = FILESIZE;
+
+  /* process command line args */
+  opterr = 0;
+  optind = 0;
+      do {
+      i=getopt(argc,argv,opt_string);
+      switch (i) {
+	  case 'd': strncpy(dirname,optarg,FILENAME_MAX-1);  break;
+	  case 'L': fcount=atoi(optarg) + 1; break;
+	  case 'F': break;
+	  case 'r': strncpy(rootname,optarg,ROOTLEN-1);  break;
+	  case 'N': filesperdir=atoi(optarg); break;
+	  case 'w': wflag=1; break;
+	  case 'z': maxfilesize=atol(optarg);
+		    if (strpbrk(optarg,"kK")) maxfilesize*=K;
+		    break;
+	  case '?': msg(MSG_EXIT_ABNORM,"Invalid option -%c",optopt);
+	  default: break;
+      }
+  }  while (i!=-1);
+
+  /* see if files already exist */
+  if (wflag) {
+      scanf("%*d %d %d %s %s",&fcount,&i,dirname,rootname);
+      fcount++;
+      if (i) filesperdir=i;
+  }
+
+  /* check dirname for existance and access for effective user id */
+  if ( (i=open(dirname,O_RDONLY) ) ==-1)
+      msg(MSG_EXIT_ABNORM,"error opening %s for r/w",dirname);
+  else close(i);
+}
+
 main( int argc, char **argv) {
-/* getopt variables */
-extern char *optarg;
-extern int optind, opterr, optopt;
+  oui_init_options(argc, argv);
 
-/* local variables */	
-int  i, wflag;
-
-    /* initialise msg options from command line */
-    msg_init_options(HDR,argc,argv);
-    BEGIN_MSG;
-    cc_init_options(argc,argv,0,0,0,0,NOTHING_ON_QUIT);
-
-    /* initialisations */
-    strcpy(rootname,ROOTNAME);
-    getcwd(dirname,FILENAME_MAX);
-    wflag=0;
-    logging = 1;
-    fcount = 0;
-    filesperdir = FILESPERDIR;
-    maxfilesize = FILESIZE;
-
-    /* process command line args */
-    opterr = 0;
-    optind = 0;
-	do {
-	i=getopt(argc,argv,opt_string);
-	switch (i) {
-	    case 'd': strncpy(dirname,optarg,FILENAME_MAX-1);  break;
-	    case 'L': fcount=atoi(optarg) + 1; break;
-	    case 'F': break;
-	    case 'r': strncpy(rootname,optarg,ROOTLEN-1);  break;
-	    case 'N': filesperdir=atoi(optarg); break;
-	    case 'w': wflag=1; break;
-	    case 'z': maxfilesize=atol(optarg);
-			if (strpbrk(optarg,"kK")) maxfilesize*=K;
-			break;
-	    case '?': msg(MSG_EXIT_ABNORM,"Invalid option -%c",optopt);
-	    default: break;
-	}
-    }  while (i!=-1);
-
-
-    /* see if files already exist */
-    if (wflag) {
-	scanf("%*d %d %d %s %s",&fcount,&i,dirname,rootname);
-	fcount++;
-	if (i) filesperdir=i;
-    }
-
-    /* check dirname for existance and access for effective user id */
-    if ( (i=open(dirname,O_RDONLY) ) ==-1)
-	msg(MSG_EXIT_ABNORM,"error opening %s for r/w",dirname);
-    else close(i);
-
-    /* initialise logger into DRing */
-    if (DC_init_options(argc,argv) != 0) 
-	msg(MSG_EXIT_ABNORM,"Can't initialise into DBR");
-
-    /* check that file size is within limits */
-    if (maxfilesize > MAXFILESIZE || maxfilesize < (dbr_info.max_rows*tmi(nbrow) + dbr_info.nrowminf*tmi(nbrow) + sizeof(tstamp_type))) {
+  /* check that file size is within limits */
+  if (maxfilesize > MAXFILESIZE ||
+	  maxfilesize < (dbr_info.max_rows*tmi(nbrow) +
+					dbr_info.nrowminf*tmi(nbrow) +
+					sizeof(tstamp_type))) {
 	msg(MSG,"Can't have filesize %ld, defaulted to %d",maxfilesize, FILESIZE);
 	maxfilesize=FILESIZE;
-    }
-    /* check files/dir is within limits */
-    if (filesperdir > MAXFILESPERDIR || filesperdir <= 0) {
-	msg(MSG,"Can't have files/dir %d, defaulted to %d",filesperdir, FILESPERDIR);
-	filesperdir=FILESPERDIR;
-    }
+  }
 
-    /* main loop of command/data transmission around ring */
-    DC_operate();
+  /* check files/dir is within limits */
+  if (filesperdir > MAXFILESPERDIR || filesperdir <= 0) {
+	msg(MSG,"Can't have files/dir %d, defaulted to %d",
+		filesperdir, FILESPERDIR);
+	filesperdir=FILESPERDIR;
+  }
+
+  /* main loop of command/data transmission around ring */
+  DC_operate();
 }
