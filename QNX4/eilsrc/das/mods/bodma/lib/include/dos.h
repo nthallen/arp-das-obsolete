d4074a87bd4f1980ddbd33987253ffa1fe4f04dd
Norton Allen <allen@huarp.harvard.edu>
2007-01-03 21:31:34 +0000

    Source files

---
+++ QNX4/eilsrc/das/mods/bodma/lib/include/dos.h
@@ -0,0 +1,2 @@
+#include <i86.h>
+#include "my_qnx.h"
---
+++ QNX4/eilsrc/das/mods/bodma/lib/include/my_qnx.h
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/seginfo.h>
+#include <sys/types.h>
+#include <sys/inline.h>
+#include <sys/irqinfo.h>
+#include <malloc.h>
+#ifndef max
+#define max __max
+#endif
+#ifndef min
+#define min __min
+#endif
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+
+
+
+
