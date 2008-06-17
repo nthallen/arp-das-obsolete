5fe94dd3ce6b6c6dbef436c891bd293a53cf1da4
Norton Allen <allen@huarp.harvard.edu>
2008-06-17 18:50:23 +0000

    Functioning test of timing via timer_create(), pulses and MsgReceive()

--- QNX6/DGdev/test_timer.c
+++ QNX6/DGdev/test_timer.c
@@ -1,19 +1,30 @@
 /* Timer test
    Create a channel
-   Create a timer
-   Create a pulse
+   Create a connection to the channel
+   Define an event as Pulse to connection
+   Create a timer using event
+   Set the timer
    MsgReceive() waiting for the pulse and print something
 */
 #include <signal.h>
 #include <time.h>
-#define MY_PULSE_CODE 7
+#include <sys/neutrino.h>
+#include <sys/netmgr.h>
+
+#define MY_PULSE_CODE _PULSE_CODE_MINAVAIL
+
+typedef union {
+  struct _pulse pulse;
+  /* your other message structures would go here too */
+} my_message_t;
 
 int main(int argc, char **argv) {
   int rc;
   struct sigevent ev;
   timer_t timerid;
-  int chid;
-  struct itimerspec       itime;
+  int chid, rcvid;
+  struct itimerspec itime;
+  my_message_t msg;
 
   chid = ChannelCreate(0);
 
@@ -31,7 +42,7 @@ int main(int argc, char **argv) {
   itime.it_interval.tv_sec = 1;
   /* 500 million nsecs = .5 secs */
   itime.it_interval.tv_nsec = 500000000; 
-  timer_settime(timer_id, 0, &itime, NULL);
+  timer_settime(timerid, 0, &itime, NULL);
   for (;;) {
     rcvid = MsgReceive(chid, &msg, sizeof(msg), NULL);
     if (rcvid == 0) { /* we got a pulse */
