8585ec71a508beef9b070d5190455460281aa960
Norton Allen <allen@huarp.harvard.edu>
2010-08-09 19:33:47 +0000

    file documentation

--- QNX6/subbusd/subbusd_int.h
+++ QNX6/subbusd/subbusd_int.h
@@ -3,6 +3,12 @@
 #include <sys/dispatch.h>
 #include "subbusd.h"
 
+/** \file
+  * \brief Defines interface between QNX6 resource manager and subbus hardware
+  *
+  * A different hardware architecture can be supported by reimplementing these
+  * functions.
+  */
 extern void incoming_sbreq( int rcvid, char *req );
 extern void init_subbus(dispatch_t *dpp );
 extern void shutdown_subbus(void);
